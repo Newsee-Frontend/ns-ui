3c15d6907689caaaf2e7aebec41df424413216f0
xilanhuadechuntian <Lensgcx@163.com>
2018-06-27 16:29:40 +0800

    utils
    utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,27 @@
-import {getQuery} from "../lib/utils";
+/**
+ * judge type accurate
+ * @param value
+ * @returns {*}
+ */
+export function judgeType(value) {
+  const t = Object.prototype.toString.call(value);
+  let map = {
+    '[object Boolean]': 'boolean',
+    '[object Number]': 'number',
+    '[object String]': 'string',
+    '[object Function]': 'function',
+    '[object Array]': 'array',
+    '[object Date]': 'date',
+    '[object RegExp]': 'regExp',
+    '[object Undefined]': 'undefined',
+    '[object Null]': 'null',
+    '[object Object]': 'object'
+  };
+  if (value instanceof Element) {
+    return 'element';
+  }
+  return map[t];
+}
 
 /**
  * judge array contain another Obj
@@ -19,6 +42,26 @@ export function arrContainObj(arr, obj) {
   else {
     this.throw("the object of the judgment must be a array format ，you better find it");
   }
+}
+
+/**
+ * get query
+ * @param url
+ * @param name
+ * @returns {null}
+ */
+export function getQuery(url, name) {
+  let reg = new RegExp("(^|\\?|&)" + name + "=([^&]*)(\\area-linkage|&|$)", "i");
+  if (url) {
+    if (reg.test(url)) {
+      const val = unescape(RegExp.$2.replace(/\+/g, " "));
+
+      if (val !== null && val.toString().length >= 1) {
+        return val;
+      }
+    }
+  }
+  return null;
 }
 
 /**
