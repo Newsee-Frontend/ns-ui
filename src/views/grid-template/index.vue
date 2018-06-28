e0a37168685e66e7258b58239e679a65c7685d8d
xilanhuadechuntian <Lensgcx@163.com>
2018-06-28 09:15:00 +0800

    grid-template
    grid-template

--- src/views/grid-template/index.vue
+++ src/views/grid-template/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="grid-template">
+    <div class="front-Logo">
+      CX-GRID
+      <i>Design</i>
+    </div>
     <div class="template-btn">
-      <el-button type="primary" size="small" @click="shower(basicVisible)">基础用法示例</el-button>
-      <el-button type="primary" size="small" @click="shower(bussinessVisible)">业务二次封装示例</el-button>
-      <el-button type="primary" size="small" @click="shower(testVisible)">测试操作示例</el-button>
+      <el-button type="info" plain @click="shower(basicVisible)">基础用法示例</el-button>
+      <el-button type="info" plain @click="shower(bussinessVisible)">业务二次封装示例</el-button>
+      <el-button type="info" plain @click="shower(testVisible)">测试操作示例</el-button>
     </div>
     <basic-grid-template :visible="basicVisible" v-if="basicVisible.visible"></basic-grid-template>
     <bussiness-grid-template :visible="bussinessVisible" v-if="bussinessVisible.visible"></bussiness-grid-template>
@@ -39,10 +43,24 @@
 <style rel="stylesheet/scss" lang="scss">
   .grid-template {
     position: absolute;
-    top: 40%;
+    top: 30%;
     width: 100%;
     .template-btn {
       text-align: center;
+    }
+    .front-Logo {
+      text-align: center;
+      font-size: 48px;
+      font-style: normal;
+      color: #ffffff;
+      padding: 38px 0 30px;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select:none;
+      i {
+        font-size: 40px;
+      }
     }
   }
 </style>
