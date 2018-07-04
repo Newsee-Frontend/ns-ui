59750f271dae53d080d8ecea04f8e44cabcfdcbe
xilanhuadechuntian <Lensgcx@163.com>
2018-07-04 12:39:40 +0800

    webpack.prod.conf
    webpack.prod.conf

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -71,10 +71,10 @@ const webpackConfig = merge(baseWebpackConfig, {
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
       cssProcessorOptions: config.build.productionSourceMap
-        ? { safe: true, map: { inline: false } }
-        : { safe: true }
+        ? {safe: true, map: {inline: false}}
+        : {safe: true}
     }),
-    new CleanWebpackPlugin(['dist'])
+    new CleanWebpackPlugin(['dist']),
     // // generate dist index.html with correct asset hash for caching.
     // // you can customize output by editing /index.html
     // // see https://github.com/ampedandwired/html-webpack-plugin
@@ -126,14 +126,14 @@ const webpackConfig = merge(baseWebpackConfig, {
     //   minChunks: 3
     // }),
     //
-    // // copy custom static assets
-    // new CopyWebpackPlugin([
-    //   {
-    //     from: path.resolve(__dirname, '../static'),
-    //     to: config.build.assetsSubDirectory,
-    //     ignore: ['.*']
-    //   }
-    // ])
+    // copy custom static assets
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, '../static'),
+        to: config.build.assetsSubDirectory,
+        ignore: ['.*']
+      }
+    ])
   ]
 });
 
--- src/lib/packages/base-grid/src/error-unit/error-prompt-jsx.js
+++ src/lib/packages/base-grid/src/error-unit/error-prompt-jsx.js
@@ -13,7 +13,7 @@ export default {
         case 'noData':
           return (
             <div class={"error-prompt"}>
-              <img class="error-prompt_img" src='/static/img/null.jpg'/>,
+              <img class="error-prompt_img" src='../../../../../../static/img/null.jpg'/>,
               <div class="error-prompt_content">
                 <span>暂无数据</span>
                 {/*<span className="" on-click={}>去新增</span>*/}
@@ -23,7 +23,7 @@ export default {
         case 'noResult':
           return (
             <div class="error-prompt">
-              <img class="error-prompt_img" src="/static/img/searchNone.jpg"/>
+              <img class="error-prompt_img" src="../../../../../../static/img/searchNone.jpg"/>
               <div class="error-prompt_content">
                 <span>抱歉,没有搜索到你要的结果</span>
               </div>
@@ -32,7 +32,7 @@ export default {
         case 'error':
           return (
             <div class="error-prompt trans">
-                <img class="error-prompt_img fl" src="/static/img/error.jpg"/>
+                <img class="error-prompt_img fl" src="../../../../../../static/img/error.jpg"/>
                 <div class="error-prompt_content fl">
                   <p>服务器出错</p>
                   <span>蛋定！伸个懒腰喝杯水，过会再试试吧</span>
