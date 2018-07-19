a5909da96c8bdcfee4954f2727723a6bc3a352da
xilanhuadechuntian <Lensgcx@163.com>
2018-07-19 21:21:25 +0800

    bussiness-grid-template
    bussiness-grid-template

--- src/views/grid-template/bussiness-grid/bussiness-grid-template.vue
+++ src/views/grid-template/bussiness-grid/bussiness-grid-template.vue
@@ -15,7 +15,7 @@
         <span @click="click">改变表格高度</span>
       </div>
       <!--grid-->
-      <bussiness-grid :gridID="gridID" :gridData="gridData" firstColType="selection" handleColType="handle"
+      <bussiness-grid :gridID="gridID" :gridData="gridData" firstColType="radio" handleColType="handle"
                       :loadState="loadState" :searchConditions="searchConditions"
                       :holderInfo="holderInfo"
                       :sizeInfo="sizeInfo"
