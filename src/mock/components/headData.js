5a986f5d629825c7039b4431943ecac3e6f7b262
xilanhuadechuntian <Lensgcx@163.com>
2018-07-20 19:06:07 +0800

    headData
    headData

--- src/mock/components/headData.js
+++ src/mock/components/headData.js
@@ -19,12 +19,42 @@ export default [
     "eidtConfig": null
   },
   {
+    "resourcecolumnName": "使用人",//label
+    "resourcecolumnNameEn": "ownerName",//model-key
+    "resourcecolumnCode": "ownerName",
+    "resourcecolumnXtype": "text",//筛选列 类型
+    "resourcecolumnWidth": "100",//列的宽度
+    "resourcecolumnOrder": "11",//列的序号
+    "resourcecolumnHidden": "0", //是否隐藏
+  },
+  {
+    "resourcecolumnName": "性别",//label
+    "resourcecolumnNameEn": "sex",//model-key
+    "resourcecolumnCode": "sex",
+    "resourcecolumnXtype": "text",//筛选列 类型
+    "resourcecolumnWidth": "200",//列的宽度
+    "resourcecolumnOrder": "3",//列的序号
+    "resourcecolumnHidden": "0", //是否隐藏
+    "eidtConfig": {
+      "switchType": true,
+      "type": "select",
+      "decimal": 2,
+      "disabled": false,
+      "maxlength": null,
+      "max": 80,
+      "min": 0,
+      "placeHolder": '请选择性别',
+      "require": true,
+      "validateRule": null
+    },
+  },
+  {
     "resourcecolumnName": "成立日期",//label
     "resourcecolumnNameEn": "date",//model-key
     "resourcecolumnCode": "date",
     "resourcecolumnXtype": "text",//筛选列 类型
     "resourcecolumnWidth": "150",//列的宽度
-    "resourcecolumnOrder": "3",//列的序号
+    "resourcecolumnOrder": "7",//列的序号
     "resourcecolumnHidden": "0", //是否隐藏
     "eidtConfig": {
       "switchType": true,
@@ -40,12 +70,43 @@ export default [
     },
   },
   {
+    "resourcecolumnName": "收费标准2",//label
+    "resourcecolumnNameEn": "chargeStandard2",//model-key
+    "resourcecolumnCode": "chargeStandard2",
+    "resourcecolumnXtype": "text",//筛选列 类型
+    "resourcecolumnWidth": "160",//列的宽度
+    "resourcecolumnOrder": "6",//列的序号
+    "resourcecolumnHidden": "0", //是否隐藏
+    "eidtConfig": {
+      "switchType": true,
+      "type": "select-unit",
+      "decimal": 2,
+      "disabled": false,
+      "maxlength": null,
+      "max": 2,
+      "min": 0,
+      "placeHolder": '请选择收费客户（多选）',
+      "require": true,
+      "validateRule": null
+    },
+  },
+  {
+    "resourcecolumnName": "单价",//label
+    "resourcecolumnNameEn": "unit",//model-key
+    "resourcecolumnCode": "unit",
+    "resourcecolumnXtype": "text",//筛选列 类型
+    "resourcecolumnWidth": "80",//列的宽度
+    "resourcecolumnOrder": "6",//列的序号
+    "resourcecolumnHidden": "0", //是否隐藏
+    "eidtConfig": null
+  },
+  {
     "resourcecolumnName": "总金额",//label
     "resourcecolumnNameEn": "sum",//model-key
     "resourcecolumnCode": "sum",
     "resourcecolumnXtype": "number",//筛选列 类型
     "resourcecolumnWidth": "100",//列的宽度
-    "resourcecolumnOrder": "4",//列的序号
+    "resourcecolumnOrder": "8",//列的序号
     "resourcecolumnHidden": "0", //是否隐藏
   },
   {
@@ -54,7 +115,7 @@ export default [
     "resourcecolumnCode": "actualSum",
     "resourcecolumnXtype": "number",//筛选列 类型
     "resourcecolumnWidth": "100",//列的宽度
-    "resourcecolumnOrder": "5",//列的序号
+    "resourcecolumnOrder": "9",//列的序号
     "resourcecolumnHidden": "0", //是否隐藏
   },
   {
@@ -63,7 +124,7 @@ export default [
     "resourcecolumnCode": "TaxRate",
     "resourcecolumnXtype": "text",//筛选列 类型
     "resourcecolumnWidth": "120",//列的宽度
-    "resourcecolumnOrder": "6",//列的序号
+    "resourcecolumnOrder": "10",//列的序号
     "resourcecolumnHidden": "0", //是否隐藏
     "eidtConfig": {
       "switchType": true,
@@ -79,13 +140,46 @@ export default [
     },
   },
   {
-    "resourcecolumnName": "使用人",//label
-    "resourcecolumnNameEn": "ownerName",//model-key
-    "resourcecolumnCode": "ownerName",
+    "resourcecolumnName": "收费客户（多选）",//label
+    "resourcecolumnNameEn": "chargeCustomer2",//model-key
+    "resourcecolumnCode": "chargeCustomer2",
     "resourcecolumnXtype": "text",//筛选列 类型
-    "resourcecolumnWidth": "120",//列的宽度
-    "resourcecolumnOrder": "7",//列的序号
+    "resourcecolumnWidth": "300",//列的宽度
+    "resourcecolumnOrder": "5",//列的序号
     "resourcecolumnHidden": "0", //是否隐藏
+    "eidtConfig": {
+      "switchType": true,
+      "type": "checkbox",
+      "decimal": 2,
+      "disabled": false,
+      "maxlength": null,
+      "max": 2,
+      "min": 0,
+      "placeHolder": null,
+      "require": true,
+      "validateRule": null
+    },
+  },
+  {
+    "resourcecolumnName": "是否锁定（单选）",//label
+    "resourcecolumnNameEn": "isLocked",//model-key
+    "resourcecolumnCode": "isLocked",
+    "resourcecolumnXtype": "text",//筛选列 类型
+    "resourcecolumnWidth": "100",//列的宽度
+    "resourcecolumnOrder": "4",//列的序号
+    "resourcecolumnHidden": "0", //是否隐藏
+    "eidtConfig": {
+      "switchType": true,
+      "type": "radio",
+      "decimal": 2,
+      "disabled": false,
+      "maxlength": null,
+      "max": null,
+      "min": null,
+      "placeHolder": null,
+      "require": true,
+      "validateRule": null
+    },
   },
   {
     "resourcecolumnName": "省份",//label
@@ -93,7 +187,7 @@ export default [
     "resourcecolumnCode": "province",
     "resourcecolumnXtype": "text",//筛选列 类型
     "resourcecolumnWidth": "120",//列的宽度
-    "resourcecolumnOrder": "8",//列的序号
+    "resourcecolumnOrder": "12",//列的序号
     "resourcecolumnHidden": "0", //是否隐藏
   },
   {
@@ -102,7 +196,7 @@ export default [
     "resourcecolumnCode": "city",
     "resourcecolumnXtype": "text",//筛选列 类型
     "resourcecolumnWidth": "100",//列的宽度
-    "resourcecolumnOrder": "9",//列的序号
+    "resourcecolumnOrder": "13",//列的序号
     "resourcecolumnHidden": "0", //是否隐藏
   },
   {
@@ -111,7 +205,7 @@ export default [
     "resourcecolumnCode": "address",
     "resourcecolumnXtype": "text",//筛选列 类型
     "resourcecolumnWidth": "200",//列的宽度
-    "resourcecolumnOrder": "10",//列的序号
+    "resourcecolumnOrder": "14",//列的序号
     "resourcecolumnHidden": "0", //是否隐藏
   },
   {
@@ -120,7 +214,7 @@ export default [
     "resourcecolumnCode": "zip",
     "resourcecolumnXtype": "text",//筛选列 类型
     "resourcecolumnWidth": "100",//列的宽度
-    "resourcecolumnOrder": "11",//列的序号
+    "resourcecolumnOrder": "15",//列的序号
     "resourcecolumnHidden": "0", //是否隐藏
   },
   {
@@ -129,7 +223,7 @@ export default [
     "resourcecolumnCode": "Remark",
     "resourcecolumnXtype": "text",//筛选列 类型
     "resourcecolumnWidth": "260",//列的宽度
-    "resourcecolumnOrder": "12",//列的序号
+    "resourcecolumnOrder": "16",//列的序号
     "resourcecolumnHidden": "0", //是否隐藏
     "eidtConfig": {
       "switchType": true,
@@ -139,7 +233,7 @@ export default [
       "maxlength": null,
       "max": 0,
       "min": 0,
-      "placeHolder": null,
+      "placeHolder": '请输入备注',
       "require": true,
       "validateRule": null
     },
