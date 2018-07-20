654fd69660f7c6fa201b306f959d66c3818a64d6
xilanhuadechuntian <Lensgcx@163.com>
2018-07-20 19:05:46 +0800

    baseModel
    baseModel

---
+++ src/lib/packages/base-grid/src/slot-scope/baseModel.js
@@ -0,0 +1,48 @@
+const selectUnit = {
+  "picked": {
+    "unit": "",
+    "value": ""
+  },
+  "options": [
+    {
+      "unit": "0",
+      "label": "234234432",
+      "value": 82
+    }
+  ]
+};
+
+const select = {
+  "picked": {
+    "value": ""
+  },
+  "options": [
+    {
+      "label": "234234432",
+      "value": 82
+    }
+  ]
+};
+
+const checkbox = {
+  "picked": {
+    "value": ""
+  },
+  "options": [
+    {
+      "label": "234234432",
+      "value": 82
+    }
+  ]
+};
+const radio = {
+  "picked": {
+    "value": ""
+  },
+  "options": [
+    {
+      "label": "234234432",
+      "value": 82
+    }
+  ]
+};
