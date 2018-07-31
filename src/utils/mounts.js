6ae435a54c051974d2554b21f099aabcbe936ff4
xilanhuadechuntian <Lensgcx@163.com>
2018-07-31 17:57:45 +0800

    mounts
    mounts

--- src/utils/mounts.js
+++ src/utils/mounts.js
@@ -9,13 +9,20 @@
 /*=====================================================================================================================*/
 
 import {gridDataFetch} from '../api/table/table'
+import {checkRange} from '../utils/index'
+
 export default {
   install: function (Vue, options) {
     Vue.prototype.grid = {
       //请求表格的内容数据
+      /**
+       * grid fetch
+       * @param requestData
+       * @param successcb
+       * @param errorcb
+       */
       fetch: (requestData, successcb, errorcb) => {
         // $store.dispatch('gridDataFetch', {gridID, url, query});
-
         gridDataFetch(requestData.url, requestData.query)
           .then(res => {
             successcb(res.resultData);
@@ -25,7 +32,64 @@ export default {
             errorcb(res);
           })
       },
-    }
+      /**
+       * grid validate
+       * @param gridCheck
+       * @returns {Promise<any>}
+       */
+      validate: (gridCheck) => {
+        return new Promise((resolve, reject) => {
+          try {
+            gridCheck.state = 'all-check';
+            let status = false;
+            let submitHandle = () => {
+              status = checkRange('.grid-container', '.cx-is-error');
+              if (status) {
+                window.clearTimeout(submitHandle);
+                resolve(gridCheck);
+              }
+              else {
+                window.clearTimeout(submitHandle);
+                reject(gridCheck);
+              }
+            };
+            setTimeout(submitHandle, 0);
+          }
+          catch (e) {
+            // statements to handle any exceptions
+            throw 'Please check the automatic grid validation method. There are some errors in the internal code.'
+          }
+        })
+      },
+
+      /**
+       * grid reValidate
+       * @param gridCheck
+       * @returns {Promise<any>}
+       */
+      reValidate: (gridCheck) => {
+        return new Promise((resolve, reject) => {
+          try {
+            gridCheck.state = 'empty-check-list';
+            let resetHandle = () => {
+              if (gridCheck.list.length === 0) {
+                window.clearTimeout(resetHandle);
+                resolve(gridCheck);
+              }
+              else {
+                window.clearTimeout(resetHandle);
+                reject(gridCheck);
+              }
+            };
+            setTimeout(resetHandle, 0);
+          }
+          catch (e) {
+            // statements to handle any exceptions
+            throw 'Please check the automatic grid validation method. There are some errors in the internal code.'
+          }
+        })
+      },
+    };
   }
 };
 
