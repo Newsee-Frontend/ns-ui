6ba3043208763ee4423ea967d25812c4a9c5798f
xilanhuadechuntian <Lensgcx@163.com>
2018-08-16 11:11:06 +0800

    canvas-back
    canvas-back

--- src/components/canvas-back/index.vue
+++ src/components/canvas-back/index.vue
@@ -4,12 +4,16 @@
     <glittering-stars :width="width" :height="height" v-if="type === 'stars'"></glittering-stars>
     <spot-wave :width="width" :height="height" :particleCount="particleCount" :background="background"
                :particle="particle" v-if="type === 'wave'"></spot-wave>
+    <matrix-raindrop :canvasWidth="width" :canvasHeight="height" :fontSize="fontSize" :speed="speed"
+                     :fontFamily="fontFamily" :textContent="textContent" :textColor="textColor"
+                     :backgroundColor="backgroundColor" v-if="type === 'raindrop'"></matrix-raindrop>
     <slot></slot>
   </div>
 </template>
 <script>
   import glitteringStars from "./glittering-stars/glittering-stars.vue";
   import spotWave from "./spot-wave/spot-wave.vue";
+  import matrixRaindrop from "./matrix-raindrop/matrix-raindrop";
 
   export default {
     name: 'canvas-back',
@@ -20,11 +24,18 @@
       particleCount: {type: Number, default: 30000},
       background: {type: Number, default: 0x000000},
       particle: {type: Number, default: 0x477cc2},
+      //========== matrix-raindrop ==========
+      fontSize: {type: Number},  //下落字体大小
+      fontFamily: {type: String},  //字体类型
+      textContent: {type: String}, //字体文本内容
+      textColor: {type: String},//字体颜色
+      backgroundColor: {type: String}, //canvas背景颜色，可自定义
+      speed: {type: Number}   //下落速度
     },
     data() {
       return {}
     },
-    components: {glitteringStars, spotWave},
+    components: {glitteringStars, spotWave, matrixRaindrop},
   }
 </script>
 <style rel="stylesheet/scss" lang="scss" scoped>
