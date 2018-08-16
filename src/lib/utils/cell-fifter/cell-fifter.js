f14862d1f4ecbe5eaaaf6a2272301940ef6d25b7
xilanhuadechuntian <Lensgcx@163.com>
2018-08-16 11:10:06 +0800

    cell-fifter

---
+++ src/lib/utils/cell-fifter/cell-fifter.js
@@ -0,0 +1,20 @@
+export default {
+  /**
+   * grid cell fifter function
+   * @param val
+   * @param key
+   * @returns {*}
+   */
+  cellFifter: (val, key) => {
+    return val;
+  },
+  /**
+   * test
+   * @param val
+   * @param key
+   * @returns {*}
+   */
+  test: (val, key) => {
+    return val;
+  }
+}
