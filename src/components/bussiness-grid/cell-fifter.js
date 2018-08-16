5fadd0612f722a6ea6de60e2e67bb3687f24e990
xilanhuadechuntian <Lensgcx@163.com>
2018-08-16 11:09:30 +0800

    cell-fifter
    cell-fifter

---
+++ src/components/bussiness-grid/cell-fifter.js
@@ -0,0 +1,22 @@
+export default {
+  /**
+   * grid cell fifter function
+   * @param val
+   * @param key
+   * @returns {*}
+   */
+  cellFifter: (val, key) => {
+    if (key === 'fifter') {
+      if (val === 0) {
+        return '否'
+      }
+      else if (val === 1) {
+        return '是'
+      }
+      else {
+        return val;
+      }
+    }
+    return val;
+  }
+}
