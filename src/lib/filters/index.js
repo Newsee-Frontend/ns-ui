728dda3a8d915d1ce1947196adf21993f3c59e06
xilanhuadechuntian <Lensgcx@163.com>
2018-08-16 11:11:17 +0800

    filters
    filters

--- src/lib/filters/index.js
+++ src/lib/filters/index.js
@@ -1,23 +1,32 @@
 import {judgeType} from '../utils/index'
 
 
-// /**
-//  * Other formats convert to string format
-//  * @param val
-//  * @returns {*}
-//  * @constructor
-//  */
-// export function allToString(val) {
-//   if (judgeType(val) === 'array') {
-//     return val.join(",");
-//   }
-//   else if (judgeType(val) === 'string') {
-//     return val;
-//   }
-//   else if (judgeType(val) === 'boolean') {
-//     return val ? 'true' : 'false';
-//   }
-//   else {
-//     return val ? JSON.stringify(val) : '';
-//   }
-// }
+/**
+ * Other formats convert to string format
+ * @param val
+ * @returns {*}
+ * @constructor
+ */
+export function allToString(val) {
+  if (judgeType(val) === 'array') {
+    return val.join(",");
+  }
+  else if (judgeType(val) === 'string') {
+    return val;
+  }
+  else if (judgeType(val) === 'boolean') {
+    return val ? 'true' : 'false';
+  }
+  else {
+    return val ? JSON.stringify(val) : '';
+  }
+}
+
+/**
+ * changeVal
+ * @param val
+ * @param key
+ */
+export function changeVal(val, key) {
+
+}
