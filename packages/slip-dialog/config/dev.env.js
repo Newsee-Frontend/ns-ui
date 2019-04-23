bc40e241c2b4b2295b5c0422725ba1a9693c3efd
xilanhuadechuntian <Lensgcx@163.com>
2019-04-23 10:41:55 +0800

    del

--- packages/slip-dialog/config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const merge = require('webpack-merge');
-const prodEnv = require('./prod.env');
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  API_ROOT: '"http://192.168.1.31/api"',
-  
-});
--- packages/slip-dialog/src/rename.js
+++
@@ -1,9 +0,0 @@
-export const getStageName = function (name, prefix, link, index) {
-  // console.log("改变的前缀名称为：", prefix)
-  if (!name) throw ("the component should be set with the initial name，" +
-    "please check index: " + index + " component' name whether it's property is set");
-  if (typeof prefix !== "string" || !prefix) return name;//prefix name
-  if (typeof link !== "string") return name;//connector
-  const default_name = "default-";
-  return prefix + link + name;
-};
