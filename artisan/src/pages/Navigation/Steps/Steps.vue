93c1ab18bb442683f49a3c1b791f70a7068e56a0
zhouxia <zhouxia@servyou.com.cn>
2019-04-28 15:47:15 +0800

    完成step Signed-off-by: zhouxia <zhouxia@servyou.com.cn>

--- artisan/src/pages/Navigation/Steps/Steps.vue
+++ artisan/src/pages/Navigation/Steps/Steps.vue
@@ -1,7 +1,17 @@
 <!--UI 组件库 - Steps - 页面测试-->
 <template>
   <div class="step-wrapper">
-    dididiiiidiidi
+    <ns-steps id="step" :space="300" :active="active">
+      <ns-step title="上传文件"  icon="el-icon-upload" description="这是一段很长很长很长的描述性文字"></ns-step>
+      <ns-step title="导入数据"  description="这是一段很长很长很长的描述性文字"></ns-step>
+      <ns-step title="导入完成"  description="这是一段很长很长很长的描述性文字"></ns-step>
+    </ns-steps>
+
+    <ns-steps :active="1" finish-status="success" simple style="width: 600px">
+      <ns-step title="步骤 1" ></ns-step>
+      <ns-step title="步骤 2" ></ns-step>
+      <ns-step title="步骤 3" ></ns-step>
+    </ns-steps>
   </div>
 </template>
 
@@ -9,7 +19,9 @@
   export default {
     name: '',
     data() {
-      return {};
+      return {
+        active: 2
+      };
     },
     computed: {},
     created() {
