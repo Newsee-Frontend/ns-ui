adc4b842e7c4a068c5f45a754535aedbdc654d61
xilanhuadechuntian <Lensgcx@163.com>
2019-04-30 11:55:28 +0800

    PieChart
    PieChart

--- artisan/src/pages/Others/Fullscreen-dialog/Fullscreen-dialog.vue
+++ artisan/src/pages/Others/Fullscreen-dialog/Fullscreen-dialog.vue
@@ -1,7 +1,6 @@
 <!--UI 组件库 - Fullscreen-dialog - 页面测试-->
 <template>
   <div>
-    <div id="test1" style="width: 400px;height: 200px"></div>
     <el-button @click="buttonClick">点击打开</el-button>
 
     <ns-fullscreen-dialog
@@ -28,7 +27,6 @@
 </template>
 
 <script>
-  import nscharts from './nscharts';
 
   export default {
     name: 'fullscreen-dialog-demo',
@@ -40,36 +38,6 @@
         closeOnPressEscape: true,
         lockScroll: false,
         showClose: true,
-        data: [
-          {
-            'actualTarget': '4.14',
-            'targetItem': '住宅',
-          },
-          {
-            'actualTarget': '48.21',
-            'targetItem': '学校',
-          },
-          {
-            'actualTarget': '14.96',
-            'targetItem': '写字楼',
-          },
-          {
-            'actualTarget': '13.42',
-            'targetItem': '环卫',
-          },
-          {
-            'actualTarget': '6.28',
-            'targetItem': '医院',
-          },
-          {
-            'actualTarget': '2.06',
-            'targetItem': '产业园区',
-          },
-          {
-            'actualTarget': '10.93',
-            'targetItem': '其他',
-          },
-        ],
       };
     },
     methods: {
@@ -92,19 +60,6 @@
       },
     },
     mounted() {
-      nscharts.pieChart(this.data, 'test1', {
-        centerPosition: ['30%', '50%'],
-        radiusScale: ['52%', '68%'],
-        legendPosition: ['56%', '26%'],
-        textPosition: ['29%', '44%'],
-        text: ['各业态', '收入占比'],
-        centerFontSize: 14,
-        legendMaxRowNum: 4,
-        tooltipUnit: '元',
-        clickFn: function(result) {
-          alert(result.name + '\n' + result.value + '万元 (' + result.percent + '%)');
-        },
-      });
     },
   };
 </script>
