88bfd785ac57933c765b4dbbcbf101111018e961
xilanhuadechuntian <Lensgcx@163.com>
2019-05-06 11:04:08 +0800

    env.param.config.js
    env.param.config.js

--- env.param.config.js
+++ env.param.config.js
@@ -1,54 +1,13 @@
 'use strict';
-// Template version: 1.3.1
-// see http://vuejs-templates.github.io/webpack for documentation.
-
 const path = require('path');
 
 module.exports = {
   dev: {
-    // Paths
-    assetsPublicPath: '/',// 编译发布的根目录，可配置为资源服务器域名或 CDN 域名
-    assetsSubDirectory: 'static',// 编译输出的二级目录
-    proxyTable: {},
-    contentBase: path.join(__dirname, '../dist'),       //"./dist/"
-    host: 'localhost',
-    port: 8022,
-    inline: true,
-    autoOpenBrowser: true,
-    errorOverlay: true,
-    hot: true,
-    quiet: true,
-    /**
-     * Source Maps
-     */
-    devtool: 'cheap-module-eval-source-map',
-    notifyOnErrors: true,
-    poll: false,
-    useEslint: true,
-    showEslintErrorsInOverlay: false,
-    usePostCSS: true,
-    /** Source Maps */
-    devCssSourceMap: false,
-    devJsSourceMap: false,
-    cacheBusting: true,
-  },
-  build: {
-    // Template for index.html
-    index: path.resolve(__dirname, './dist/index.html'),
-    assetsPublicPath: '/', // 编译发布的根目录，可配置为资源服务器域名或 CDN 域名
-    assetsRoot: path.resolve(__dirname, './dist'),// 编译输出的静态资源路径
-    assetsSubDirectory: 'static',// 编译输出的二级目录
-    devtool: '#source-map',
-    productionGzip: false,
-    productionGzipExtensions: ['js', 'css'],
-    bundleAnalyzerReport: process.env.npm_config_report || false,
-    extract: true,
-    usePostCSS: true,
-    useEslint: true,
-    /** Source Maps */
-    prodCssSourceMap: false,
-    prodJsSourceMap: false,
-    cacheBusting: true,
+    entry: './artisan/src/main.js',
+    templateSPA: './artisan/index.html',
+    staticPath: './artisan/static',
+    port: 8066,
+    useEslint: false,
   },
   plugin: {
     style: {
@@ -84,20 +43,17 @@ module.exports = {
       'Only-MMDD-picker',
       'Time-interval',
     ],
-    staticPath: './static',
   },
   base: {
-    entry: {
-      app: './artisan/src/main.js',
-    },
-    JSinclude: ['src', 'mock', 'test', 'artisan'],
-    templateSPA: './artisan/index.html',
-    staticPath: './artisan/static',
     mockPath: './artisan/mock',
-    aliasPath: [
-      { name: '@', path: path.resolve('./artisan/src') },
-    ],
-    sassResources: [],
+
+    JSBabelInclude: ['src', 'mock', 'test', 'artisan'],
+
+    alias: {
+      "vue$": "vue/dist/vue.esm.js",
+      "@": path.resolve("artisan/src"),
+    },
+
     prettier: {
       switch: false,
       files: [
