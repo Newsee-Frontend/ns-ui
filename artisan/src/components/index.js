5a3832a51d646b99bcf12fb54882d1e40ca46c10
xilanhuadechuntian <Lensgcx@163.com>
2019-05-07 15:33:34 +0800

    Update index.js

--- artisan/src/components/index.js
+++ artisan/src/components/index.js
@@ -1,5 +1,8 @@
 import BizTabsView from './BizTabsView/BizTabsViews';
+import DemoBlock from './Demo/DemoBlock';
+
 
 Vue.component('biz-tabs-views', BizTabsView);
+Vue.component('demo-block', DemoBlock);
 
 import Vue from 'vue';
