a3722e51d018a65e57095719920d7662221663b9
xilanhuadechuntian <Lensgcx@163.com>
2019-05-08 16:47:10 +0800

    111

--- build/env/webpack.prod.conf.js
+++ build/env/webpack.prod.conf.js
@@ -23,10 +23,6 @@ const prodWebpackConfig = merge(baseWebpackConfig, {
     globalObject: 'this'
   },
   performance: false,//配置如何展示性能提示。例如，如果一个资源超过 250kb，webpack 会对此输出一个警告来通知你
-  /**
-   * 如果我们想引用一个库，但是又不想让webpack打包，并且又不影响我们在程序中以CMD、AMD或者window/global全局等方式进行使用，
-   * 那就可以通过配置externals。这个功能主要是用在创建一个库的时候用的，但是也可以在我们项目开发中充分使用。
-   */
   externals: {
     vue: {
       root: 'Vue',
