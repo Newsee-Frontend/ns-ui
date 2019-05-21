311943e28dbd69337f9f10fc4dfe8938c86fc63c
xilanhuadechuntian <Lensgcx@163.com>
2019-05-21 11:20:59 +0800

    Update index.vue

--- artisan/src/pages/Dashboard/index.vue
+++ artisan/src/pages/Dashboard/index.vue
@@ -1,28 +1,19 @@
 <!--主页-->
 <template>
   <div class="Dashboard">
-    <pre>{{TestDemomodel}}</pre>
-    <TestDemo v-model="TestDemomodel"></TestDemo>
-
+    Dashboard
   </div>
 </template>
 
 <script>
-  import TestDemo from '../../../../src/packages/TestDemo/components/testContent';
+
 
   export default {
     name: 'Dashboard',
-    components: { TestDemo },
     data() {
-      return {
-        TestDemomodel: {
-          ipt: 22,
-        },
-      };
+      return {};
     },
     created() {
-      console.log(9999)
-      console.log(this.TestDemomodel)
     },
   };
 
