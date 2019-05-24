0f5a9bd1ea192069b301bfef25e6a1325d29714a
xilanhuadechuntian <Lensgcx@163.com>
2019-05-24 10:22:11 +0800

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -17,6 +17,7 @@ module.exports = {
   'plugins': [
     '@babel/plugin-transform-runtime',
     '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-object-assign',
     ['@babel/plugin-proposal-decorators', { 'legacy': true }],
     ['@babel/plugin-proposal-class-properties', { 'loose': true }],
     '@babel/plugin-syntax-jsx',
