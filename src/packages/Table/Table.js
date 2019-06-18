c52e83833921db9bdfe3724ce23c5225b7f26213
xilanhuadechuntian <Lensgcx@163.com>
2019-06-18 14:06:58 +0800

    Create Table.js

---
+++ src/packages/Table/Table.js
@@ -0,0 +1,117 @@
+import create from '../../create/create';
+import tableMain from './components/main';
+import actionSummary from './components/action/actionSummary';
+
+import column from './mixins/column';
+import refer from './mixins/refer';
+import check from './mixins/check';
+
+export default create({
+  name: 'table',
+  components: { tableMain, actionSummary },
+  mixins: [refer, column, check],
+  data() {
+    return {
+      summaryCommand: 'current',//合计模块控制指令
+    };
+  },
+  props: {
+    keyRefer: { type: Object },  //指代属性
+    gridID: { type: String },//表格ID值
+    head: { type: Array }, //表头数据
+    data: {
+      type: Array, default() {
+        return [];
+      },
+    }, //表格显示的数据
+  },
+
+  watch: {
+    head(val) {
+      console.log('head');
+      console.log(val);
+    },
+  },
+
+  render(h) {
+    const Table = () => {
+      return h(
+        `table-main`,
+        {
+          'class': this.recls('container'),
+          props: {
+            keyRefer: this.keyRefer,
+            head: this.head,
+            data: this.data,
+            checkStator: this.checkStator,
+            ...this.$attrs,
+          },
+          on: {
+            'selection-change': this.selectionChange,
+            'table-action': this.tableAction,
+            'cell-action': this.cellAction,
+            'add-row': this.addRow,
+            'delete-current-row': this.deleteCurrentRow,
+            'set-check-stator': this.setCheckStator,
+          },
+        },
+      );
+    };
+
+    return (
+      <div class={this.recls()}>
+        {
+          Table()
+        }
+        <action-summary command={this.summaryCommand}  {...{
+          on: {
+            'update:command': value => {
+              this.summaryCommand = value;
+            },
+          },
+        }}
+        />
+      </div>
+    );
+  },
+
+  methods: {
+    selectionChange(row, index) {
+      this.$emit('selection-change', row, index);
+    },
+
+    tableAction(info, scope) {
+      this.$emit('table-action', info, scope);
+    },
+
+    cellAction(scope, item) {
+      this.$emit('cell-action', scope, item);
+    },
+    /**
+     * add row to grid
+     */
+    addRow() {
+      this.$emit('add-row');
+    },
+    /**
+     * delete current row
+     * @param index
+     * @param row
+     */
+    deleteCurrentRow(index, row) {
+      this.$emit('delete-current-row', index, row);
+    },
+  },
+
+  created() {
+    console.log('表头数据：');
+    console.log(this.head);
+    console.log('表数据：');
+    console.log(this.data);
+    console.log('table - $attrs:');
+    console.log(this.$attrs);
+  },
+
+  mounted() {
+  },
+});
