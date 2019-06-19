190e08999d76602efac618e9989a3fd4af4c0d3e
xilanhuadechuntian <Lensgcx@163.com>
2019-06-19 13:49:05 +0800

    Update Biz-table.vue

--- artisan/src/components/Biz-table/Biz-table.vue
+++ artisan/src/components/Biz-table/Biz-table.vue
@@ -2,18 +2,23 @@
 <template>
   <div v-loading="!isRender">
     <ns-table ref="biz-table"
-              :data="data" :head="finalHead" :keyRefer="keyRefer" :height="400"
+              :data="data.list" :head="finalHead" :keyRefer="keyRefer" :height="400"
               :cellFifter="cellFifter"
               :showHeadOperation="showHeadOperation"
               :showSummary="showSummary"
               @selection-change="selectionChange"
               @table-action="tableAction"
               @cell-action="cellAction"
-              @form-change="formChange"
+              @cell-form-change="cellFormChange"
               @add-row="addRow"
               @delete-current-row="deleteCurrentRow"
               v-if="isRender"
     ></ns-table>
+    <ns-pagination
+      :total="data.total" :searchConditions="searchConditions"
+      @size-change="sizeChange"
+      @current-change="currentChange"
+    ></ns-pagination>
   </div>
 </template>
 
@@ -33,7 +38,10 @@
       };
     },
     props: {
-      data: { type: Array, default: [] },
+      data: { type: Object },
+      isLocalHead: { type: Boolean, default: false }, //是否为本地表头数据
+      head: { type: Array }, //表头数据
+      //表格数据加载状态
       loadState: {
         type: Object, default() {
           return {
@@ -41,9 +49,11 @@
             head: false,
           };
         },
-      },  //表格数据加载状态
-      firstColType: { type: String, default: 'selection', validator: t => ['index', 'selection', 'radio', null].indexOf(t) > -1 },//第一列固定列类型（非自动表头配置）
-      hasActionCol: { type: Boolean, default: true },
+      },
+      searchConditions: { type: Object },//筛选条件
+      //第一列固定列类型（非自动表头配置）
+      firstColType: { type: String, default: 'selection', validator: t => ['index', 'selection', 'radio', null].indexOf(t) > -1 },
+      hasActionCol: { type: Boolean, default: true },//是否有操作列
       showHeadOperation: { type: Boolean, default: true },//表头设置操作模块开关
       showAddRowOperation: { type: Boolean, default: false },//表头设置 新增行操作模块开关
       showSummary: { type: Boolean, default: true },//是否显示合计行
@@ -51,35 +61,48 @@
 
 
     computed: {
-      ...mapGetters(['tableHead', 'tableData']),
+      ...mapGetters(['tableHead']),
       isRender() {
         return this.loadState.data && this.loadState.head;
       },
       finalHead() {
-        if (this.tableHead) {
-          return [
-            ...(this.firstColType ? [columnConfig[this.firstColType]] : []),
-            ...this.tableHead,
-            ...(this.hasActionCol ? [columnConfig['action']] : []),
-            ...(this.showAddRowOperation ? [columnConfig['add-row']] : []),
+        return [
+          ...(this.firstColType ? [columnConfig[this.firstColType]] : []),
+          ...(this.isLocalHead ? this.head : this.tableHead),
+          ...(this.hasActionCol ? [columnConfig['action']] : []),
+          ...(this.showAddRowOperation ? [columnConfig['add-row']] : []),
 
-          ].map(item => {
-            item.resourcecolumnHidden = item.resourcecolumnHidden === '1' ? true : false;
-            return item;
-          });
-        }
-        return [];
+        ].map(item => {
+          item.resourcecolumnHidden = item.resourcecolumnHidden === '1' ? true : false;
+          return item;
+        });
       },
     },
     created() {
-      this.$store.dispatch('generateTableHead', {}).then(() => {
+      this.$store.dispatch('generateTableHead', { funcId: 'funcId' }).then(() => {
         this.loadState.head = true;
-
       }).catch(() => {
         this.loadState.head = true;
       });
     },
     methods: {
+      /**
+       * 分页器当前显示条数改变
+       * @param val
+       */
+      sizeChange(val) {
+        console.log('size-change', val);
+        this.$emit('reload');
+      },
+      /**
+       * 分页器当前页切换
+       * @param val
+       */
+      currentChange(val) {
+        console.log('current-page-change', val);
+        this.$emit('reload');
+      },
+
       selectionChange(row, index) {
         this.$emit('selection-change', row, index);
       },
@@ -90,8 +113,8 @@
         console.log('表格单元格点击行为事件');
         console.log(scope, item);
       },
-      formChange(value, param) {
-        this.$emit('form-change', value, param);
+      cellFormChange(value, param) {
+        this.$emit('cell-form-change', value, param);
       },
       /**
        * add row to grid
@@ -121,5 +144,11 @@
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
-
+  .ns-pagination {
+    padding: 5px;
+    z-index: 99;
+    background-color: #ebedef;
+    border-left: 1px solid #e8e8e8;
+    border-right: 1px solid #e8e8e8;
+  }
 </style>
