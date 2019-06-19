5923f7c3f0657fb16f7183c37612f30a90b38b87
xilanhuadechuntian <Lensgcx@163.com>
2019-06-19 13:49:15 +0800

    Update index.js

--- artisan/src/store/index.js
+++ artisan/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+
 import User from './modules/User';
 import Drawer from './modules/Drawer';
 import Sidebar from './modules/Sidebar';
@@ -14,6 +15,7 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
+
     User,
     Drawer,
     Sidebar,
