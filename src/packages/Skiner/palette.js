9f81ed1a72d62428854f958150542581342b9af4
xilanhuadechuntian <Lensgcx@163.com>
2019-06-20 10:36:53 +0800

    Update palette.js

--- src/packages/Skiner/palette.js
+++ src/packages/Skiner/palette.js
@@ -1,12 +1,25 @@
 /**
  * palette - 调色盘
  */
-export default [
-  { key: 'blue', color: '#0A7AF8' },
-  { key: 'green', color: '#4CA770' },
-  { key: 'red', color: '#E24C57' },
-  { key: 'purple', color: '#8352AF' },
-  { key: 'black', color: '#252836' },
-  { key: 'custom', color: null },
-];
-
+export default {
+  commons: [
+    { key: 'blue', color: '#0A7AF8' },
+    { key: 'green', color: '#4CA770' },
+    { key: 'red', color: '#E24C57' },
+    { key: 'purple', color: '#8352AF' },
+    { key: 'black', color: '#252836' },
+    { key: 'custom', color: null },
+  ],
+  predefine: [
+    '#ff4500',
+    '#ff8c00',
+    '#ffd700',
+    '#90ee90',
+    '#00ced1',
+    '#1e90ff',
+    '#c71585',
+    '#000000',
+    '#cccccc',
+    '#ffffff',
+  ],
+};
