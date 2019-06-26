6bb535deee0c08edfac18906545fcb7dd924794c
xilanhuadechuntian <Lensgcx@163.com>
2019-06-26 14:21:22 +0800

    Update Upload.vue

--- artisan/src/pages/Form/Upload/Upload.vue
+++ artisan/src/pages/Form/Upload/Upload.vue
@@ -1,13 +1,27 @@
 <!--UI 组件库 - Upload - 页面测试-->
 <template>
   <div class="upload-wrapper">
-    <ns-upload
-      v-model="childItem"
-      width="100px"
-      height="100px"
-      :action="dynamicUrl"
-    >
-    </ns-upload>
+    <demo-block>
+      <template slot="title">上传控件 - Upload</template>
+      <template slot="describe">默认用法</template>
+      <template slot="content">
+        <ns-upload
+          v-model="childItem1"
+          width="300px"
+          height="300px"
+          :action="dynamicUrl"
+        >
+        </ns-upload>
+        <ns-upload
+          v-model="childItem2"
+          width="200px"
+          height="200px"
+          :action="dynamicUrl"
+        >
+        </ns-upload>
+      </template>
+    </demo-block>
+
   </div>
 </template>
 
@@ -17,9 +31,10 @@
     data() {
       return {
         dynamicUrl: '/post',
-        childItem: [{
-          fileUrl: 'http://oa.new-see.com/upload/file/public/upload/file/public/201705/IMG_0680201705230907560.JPG'
-        }]
+        childItem1: [{
+          fileUrl: 'http://oa.new-see.com/upload/file/public/upload/file/public/201705/IMG_0680201705230907560.JPG',
+        }],
+        childItem2: [],
       };
     },
     computed: {},
