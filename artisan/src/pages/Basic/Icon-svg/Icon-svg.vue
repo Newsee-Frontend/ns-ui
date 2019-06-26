c53a7a479911925459d122d53ed4a707aed0ac5a
xilanhuadechuntian <Lensgcx@163.com>
2019-06-26 14:20:59 +0800

    Update Icon-svg.vue

--- artisan/src/pages/Basic/Icon-svg/Icon-svg.vue
+++ artisan/src/pages/Basic/Icon-svg/Icon-svg.vue
@@ -1,14 +1,27 @@
 <!--UI 组件库 - Icon-svg - 页面测试-->
 <template>
   <div>
-    <ns-icon-svg icon-class="fold"></ns-icon-svg>
-    <ns-icon-svg icon-class="renwu"></ns-icon-svg>
-    <ns-icon-svg icon-class="bianji-"></ns-icon-svg>
-    <ns-icon-svg icon-class="shujugongxiang"></ns-icon-svg>
-    <ns-button type="primary">
-      搜索
-      <ns-icon-svg icon-class="sousuo" @click="svgClick"></ns-icon-svg>
-    </ns-button>
+    <demo-block>
+      <template slot="title">图标-SVG 用法</template>
+      <template slot="describe">基础用法</template>
+      <template slot="content">
+        <ns-icon-svg icon-class="fold"></ns-icon-svg>
+        <ns-icon-svg icon-class="renwu"></ns-icon-svg>
+        <ns-icon-svg icon-class="bianji-"></ns-icon-svg>
+        <ns-icon-svg icon-class="shujugongxiang"></ns-icon-svg>
+        <ns-button type="primary">
+          搜索
+          <ns-icon-svg icon-class="sousuo" @click="svgClick"></ns-icon-svg>
+        </ns-button>
+      </template>
+    </demo-block>
+    <demo-block>
+      <template slot="title">图标-SVG 用法2</template>
+      <template slot="describe">基础用法2</template>
+      <template slot="content">
+        <ns-icon-svg icon-class="bug"></ns-icon-svg>
+      </template>
+    </demo-block>
   </div>
 </template>
 
