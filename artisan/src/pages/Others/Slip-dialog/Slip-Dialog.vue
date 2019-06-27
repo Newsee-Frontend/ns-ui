ff05edd21bb1848dee19bca52c920ddfaed8f093
xilanhuadechuntian <Lensgcx@163.com>
2019-06-27 14:55:31 +0800

    Update Slip-Dialog.vue

--- artisan/src/pages/Others/Slip-dialog/Slip-Dialog.vue
+++ artisan/src/pages/Others/Slip-dialog/Slip-Dialog.vue
@@ -1,113 +1,92 @@
+<!--UI 组件库 - Slip-Dialog - 页面测试-->
 <template>
-  <div>
-    <el-form label-width="70px" :model="form" inline size="mini">
-      <!--打开-->
-      <el-form-item label-width="0">
+  <demo-block>
+    <template slot="title">Slip-Dialog - 侧滑对话框</template>
+    <template slot="describe">基础用法</template>
+    <template slot="content">
+
+      <!--参数控制-->
+      <div class="control-block form-block-line">
         <span>开关激活: </span>
-        <el-switch
+        <ns-switch
           v-model="form.dialogSw"
           active-color="#13ce66"
           inactive-color="#ff4949"
           style="margin-right: 10px"
         >开关2
-        </el-switch>
-        <el-button size="mini" @click.stop="openByRef()">使用事件打开弹窗</el-button>
-        <el-button type="danger" size="mini" @click.stop="closeByRef()">使用事件关闭弹窗</el-button>
-      </el-form-item>
-      <!--动画，位置-->
-      <el-row>
-        <el-col :span="24">
-          <el-form-item label="动画">
-            <el-select v-model="form.animation" placeholder="动画">
-              <el-option
-                v-for="item in animationList"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              ></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="进入位置">
-            <el-select v-model="form.postion" placeholder="进入位置">
-              <el-option
-                v-for="item in postionList"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              ></el-option>
-            </el-select>
-          </el-form-item>
-        </el-col>
-      </el-row>
-      <!--左右边距-->
-      <el-row>
-        <el-col :span="24">
-          <el-form-item label="左边距">
-            <el-input v-model="form.iptleft" placeholder="请输入左边距"></el-input>
-          </el-form-item>
-          <el-form-item label="右边距">
-            <el-input v-model="form.iptright" placeholder="请输入右边距"></el-input>
-          </el-form-item>
-        </el-col>
-      </el-row>
-      <!--上下边距-->
-      <el-row>
-        <el-col :span="24">
-          <el-form-item label="上边距">
-            <el-input v-model="form.ipttop" placeholder="请输入上边距"></el-input>
-          </el-form-item>
-          <el-form-item label="下边距">
-            <el-input v-model="form.iptbottom" placeholder="请输入下边距"></el-input>
-          </el-form-item>
-        </el-col>
-      </el-row>
-    </el-form>
-    <!--侧边滑入窗口-->
-    <ns-slip-dialog
-      ref="slipDialog"
-      :visible.sync="form.dialogSw"
-      customClass="slipDialog-test"
-      :left="form.iptleft"
-      :right="form.iptright"
-      :top="form.ipttop"
-      :bottom="form.iptbottom"
-      appendToBody
-      :entr-position="form.postion"
-      :animation="form.animation"
-      @before-enter="beforeEnter"
-      @enter="enter"
-      @after-enter="afterEnter"
-      @before-leave="beforeLeave"
-      @leave="leave"
-      @after-leave="afterLeave"
-      :beforeClose="beforeClose"
-    >
-      <h2 style="text-align: center;padding: 15px 0 ;margin: 0">侧边滑入窗口</h2>
-      <!--<img src="../../../assets/img/banner/banner.png" alt="banner"/>-->
-      <div style="text-align: right">
-        <el-button @click.stop="form.dialogSw2 = true">打开全屏弹窗</el-button>
-        <el-button type="danger" @click.stop="btnInDialogClick">关闭弹窗</el-button>
+        </ns-switch>
+        <ns-button @click="openByRef()">使用事件打开弹窗</ns-button>
+        <ns-button type="danger" @click="closeByRef()">使用事件关闭弹窗</ns-button>
       </div>
-    </ns-slip-dialog>
+      <div class="control-block form-block-line">
+        <span>动画: </span>
+        <ns-select v-model="form.animation" placeholder="动画" :options="animationList"></ns-select>
+        <span>进入位置: </span>
+        <ns-select v-model="form.postion" placeholder="进入位置" :options="postionList"></ns-select>
+      </div>
+      <div class="control-block form-block-line">
+        <span>左边距: </span>
+        <ns-input v-model="form.iptleft" placeholder="请输入左边距"></ns-input>
+        <span>右边距: </span>
+        <ns-input v-model="form.iptright" placeholder="请输入右边距"></ns-input>
+        <span>上边距: </span>
+        <ns-input v-model="form.ipttop" placeholder="请输入上边距"></ns-input>
+        <span>下边距: </span>
+        <ns-input v-model="form.iptbottom" placeholder="请输入下边距"></ns-input>
+      </div>
 
-    <!--全屏窗口-->
-    <ns-slip-dialog
-      ref="slipDialog2"
-      :visible.sync="form.dialogSw2"
-      customClass="slipDialog-fullScreen"
-      left="0"
-      right="0"
-      top="0"
-      bottom="0"
-      appendToBody
-    >
-      <h2 style="text-align: center;padding: 15px 0 ;margin: 0">全屏窗口</h2>
-      <!--<img src="../../../assets/img/banner/banner.png" alt="banner"/>-->
-      <div style="text-align: right">
-        <el-button @click.stop="form.dialogSw2 = false">关闭窗口</el-button>
-      </div>
-    </ns-slip-dialog>
-  </div>
+      <!--侧边滑入窗口-->
+      <ns-slip-dialog
+        ref="slipDialog"
+        :visible.sync="form.dialogSw"
+        customClass="slipDialog-test"
+        :left="form.iptleft"
+        :right="form.iptright"
+        :top="form.ipttop"
+        :bottom="form.iptbottom"
+        appendToBody
+        :entr-position="form.postion"
+        :animation="form.animation"
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @after-enter="afterEnter"
+        @before-leave="beforeLeave"
+        @leave="leave"
+        @after-leave="afterLeave"
+        :beforeClose="beforeClose"
+      >
+        <h2 style="text-align: center;padding: 15px 0 ;margin: 0">侧边滑入窗口</h2>
+        <ns-editor :height="500" v-model="content"/>
+        <div class="editor-content">
+          <h3>输入内容如下:</h3>{{content}}
+        </div>
+
+
+        <div style="text-align: right">
+          <el-button @click.stop="form.dialogSw2 = true">打开全屏弹窗</el-button>
+          <el-button type="danger" @click.stop="btnInDialogClick">关闭弹窗</el-button>
+        </div>
+      </ns-slip-dialog>
+
+      <!--全屏窗口-->
+      <ns-slip-dialog
+        ref="slipDialog2"
+        :visible.sync="form.dialogSw2"
+        customClass="slipDialog-fullScreen"
+        left="0"
+        right="0"
+        top="0"
+        bottom="0"
+        appendToBody
+      >
+        <h2 style="text-align: center;padding: 15px 0 ;margin: 0">全屏窗口</h2>
+        <!--<img src="../../../assets/img/banner/banner.png" alt="banner"/>-->
+        <div style="text-align: right">
+          <el-button @click.stop="form.dialogSw2 = false">关闭窗口</el-button>
+        </div>
+      </ns-slip-dialog>
+    </template>
+  </demo-block>
 </template>
 
 <script>
@@ -140,11 +119,8 @@
           { label: '上', value: 'top' },
           { label: '下', value: 'bottom' },
         ],
+        content: '',
       };
-    },
-    created() {
-
-
     },
     methods: {
       openByRef() {
@@ -187,4 +163,3 @@
   };
 </script>
 
-<style scoped></style>
