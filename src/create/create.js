b5a441e88851ce8f472f7d6161e873c7afbcd0ef
xilanhuadechuntian <Lensgcx@163.com>
2019-06-28 12:03:18 +0800

    Update create.js

--- src/create/create.js
+++ src/create/create.js
@@ -2,7 +2,6 @@
  * Create a component with common options
  */
 import createBasic from './create-basic';
-import '../icons/iconfont.css'
 import iconSvg from '../packages/Icon-svg/Icon-svg';
 import iconClass from '../packages/Icon-class/Icon-class';
 // import Loading from '../loading';
