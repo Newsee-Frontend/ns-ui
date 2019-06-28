0a430e2d2f8162de81ae57d2cdaac3ca59d04c05
xilanhuadechuntian <Lensgcx@163.com>
2019-06-28 12:03:10 +0800

    Update index.js

--- build/run/index.js
+++ build/run/index.js
@@ -3,7 +3,7 @@
  */
 const shell = require('shelljs');
 const signale = require('signale');
-const {Signale} = signale;
+const { Signale } = signale;
 
 const tasks = [
   'bootstrap',
@@ -26,7 +26,7 @@ const tasks = [
 tasks.forEach(task => {
   signale.start(task);
 
-  const interactive = new Signale({interactive: true});
+  const interactive = new Signale({ interactive: true });
   interactive.pending(task);
   shell.exec(`npm run ${task} --silent`);
   interactive.success(task);
