39c0aa7751c9f532c40f6d1ae806940a9bc8e9ae
xilanhuadechuntian <Lensgcx@163.com>
2019-06-28 12:03:46 +0800

    Update editor-btn.js

--- src/packages/Editor/components/editor-btn.js
+++ src/packages/Editor/components/editor-btn.js
@@ -1,7 +1,15 @@
+import IconClass from '../../Icon-class/Icon-class';
 import { stopPropagation } from '../../../utils/event';
 
 export default {
   name: 'editor-btn',
+  components: { IconClass },
+  props: {
+    iconClass: {
+      type: String,
+      default: '',
+    },
+  },
   render(h) {
     return (
       h('div',
@@ -15,8 +23,11 @@ export default {
           h(
             'button',
             [
-              h('i', {
-                  class: 'el-icon-upload  mce-ico',
+              h('IconClass', {
+                  class: 'mce-ico',
+                  props: {
+                    'icon-class': this.iconClass,
+                  },
                 },
               ),
             ],
