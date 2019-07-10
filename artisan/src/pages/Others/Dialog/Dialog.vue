03d737c659e0af51303d4c1aa1ee227a026ab893
xilanhuadechuntian <Lensgcx@163.com>
2019-07-10 16:08:06 +0800

    Update Dialog.vue

--- artisan/src/pages/Others/Dialog/Dialog.vue
+++ artisan/src/pages/Others/Dialog/Dialog.vue
@@ -1,76 +1,77 @@
 <!--UI 组件库 - Dialog - 页面测试-->
 <template>
-  <div>
-    <demo-block>
-      <template slot="title">Dialog - 对话框</template>
-      <template slot="describe">基础用法</template>
-      <template slot="content">
-        <el-button @click="buttonClick">点击打开</el-button>
-        <ns-dialog
-          :visible.sync="visible"
-          :type="type"
-          :size="size"
-          :title="title"
-          :top="top"
-          :modal="modal"
+  <div class="win">
+    <div class="ns-container">
+      <demo-block>
+        <template slot="title">Dialog - 对话框</template>
+        <template slot="describe">基础用法</template>
+        <template slot="content">
+          <el-button @click="buttonClick">点击打开</el-button>
+          <ns-dialog
+            :visible.sync="visible"
+            :type="type"
+            :size="size"
+            :title="title"
+            :top="top"
+            :modal="false"
 
-          :is-append-to-body="false"
-          :lock-scroll="lockScroll"
-          :close-on-click-modal="closeOnClickModal"
-          :close-on-press-escape="closeOnPressEscape"
+            :is-append-to-body="false"
+            :lock-scroll="lockScroll"
+            :close-on-click-modal="closeOnClickModal"
+            :close-on-press-escape="closeOnPressEscape"
 
-          :show-close="showClose"
-          :before-close="beforeClose"
-          @close="close"
-          @open="open"
-        >
-          <div>
-            <h1>dialog-demo</h1>
-            <ns-editor :height="500" v-model="content"/>
-            <div class="editor-content">
-              <h3>输入内容如下:</h3>{{content}}
-            </div>
-          </div>
-          <div slot="footer">
-            <ns-button @click="buttonClose">关闭</ns-button>
-          </div>
-        </ns-dialog>
-      </template>
-    </demo-block>
-
-    <demo-block>
-      <template slot="title">Dialog - 对话框</template>
-      <template slot="describe">嵌套用法</template>
-      <template slot="content">
-        <el-button @click=" visible2=true">点击打开</el-button>
-        <ns-dialog
-          :visible.sync="visible2"
-          type="autoHeight"
-          :size="size"
-          :title="title"
-        >
-          <div>
-            <h1>dialog-demo2</h1>
-            <ns-dialog
-              :visible.sync="visible3"
-              type="autoHeight"
-              :size="size"
-              :title="title"
-            >
-              <h1>dialog-demo3</h1>
-              <div slot="footer">
-                <ns-button @click="visible3=false">关闭</ns-button>
+            :show-close="showClose"
+            :before-close="beforeClose"
+            @close="close"
+            @open="open"
+          >
+            <div>
+              <h1>dialog-demo</h1>
+              <ns-editor :height="500" v-model="content"/>
+              <div class="editor-content">
+                <h3>输入内容如下:</h3>{{content}}
               </div>
-            </ns-dialog>
-          </div>
-          <div slot="footer">
-            <ns-button @click="visible3=true">打开二级弹窗</ns-button>
-            <ns-button @click="visible2=false">关闭</ns-button>
-          </div>
-        </ns-dialog>
-      </template>
-    </demo-block>
+            </div>
+            <div slot="footer">
+              <ns-button @click="buttonClose">关闭</ns-button>
+            </div>
+          </ns-dialog>
+        </template>
+      </demo-block>
 
+      <demo-block>
+        <template slot="title">Dialog - 对话框</template>
+        <template slot="describe">嵌套用法</template>
+        <template slot="content">
+          <el-button @click=" visible2=true">点击打开</el-button>
+          <ns-dialog
+            :visible.sync="visible2"
+            type="autoHeight"
+            :size="size"
+            :title="title"
+          >
+            <div>
+              <h1>dialog-demo2</h1>
+              <ns-dialog
+                :visible.sync="visible3"
+                type="autoHeight"
+                :size="size"
+                :title="title"
+              >
+                <h1>dialog-demo3</h1>
+                <div slot="footer">
+                  <ns-button @click="visible3=false">关闭</ns-button>
+                </div>
+              </ns-dialog>
+            </div>
+            <div slot="footer">
+              <ns-button @click="visible3=true">打开二级弹窗</ns-button>
+              <ns-button @click="visible2=false">关闭</ns-button>
+            </div>
+          </ns-dialog>
+        </template>
+      </demo-block>
+    </div>
   </div>
 </template>
 
@@ -88,7 +89,7 @@
         title: '标题',
         top: '7%',
         modal: true,
-        closeOnClickModal: true,
+        closeOnClickModal: false,
         closeOnPressEscape: true,
         lockScroll: false,
         showClose: true,
