a899d2331f88d719633daaaac447d620fe4c74bf
xilanhuadechuntian <Lensgcx@163.com>
2019-07-11 17:27:17 +0800

    Create Loading-block.js

---
+++ src/packages/Loading-block/Loading-block.js
@@ -0,0 +1,28 @@
+import create from '../../create/create';
+
+export default create({
+  name: 'loading-block',
+
+  data() {
+    return {};
+  },
+  props: {
+    simple: { type: Boolean, default: false },
+  },
+  render(h) {
+    return (
+      <div class={this.recls()}>
+        {
+          !this.simple ?
+
+            <div class={this.recls('content')}>
+
+              <div class={'loading-img'}/>
+
+            </div>
+            : null
+        }
+      </div>
+    );
+  },
+});
