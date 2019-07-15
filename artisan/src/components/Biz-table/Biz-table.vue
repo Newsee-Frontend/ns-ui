dd5a8aed1d81356d9d26aa9a0b7d22671ed6c405
xilanhuadechuntian <Lensgcx@163.com>
2019-07-15 11:36:22 +0800

    Update Biz-table.vue

--- artisan/src/components/Biz-table/Biz-table.vue
+++ artisan/src/components/Biz-table/Biz-table.vue
@@ -156,6 +156,28 @@
     },
     created() {
       this.getTableHead();
+      console.log(2222222222222222);
+      console.log(2222222222222222);
+      console.log(this.keyRefer);
+      console.log(this.rulesConfig);
+      console.log(2222222222222222);
+    },
+    beforeDestroy() {
+
+      this.head = null;
+      this.keyRefer = null;
+      this.rulesConfig = null;
+      this.cellFifter = null;
+
+      // :data="data.list" :head="finalHead" :keyRefer="keyRefer" :height="height"
+      // :loadState="loadState"
+      // :is-form-table="isFormTable"
+      // :cellFifter="cellFifter"
+      // :showHeadOperation="showHeadOperation"
+      // :showSummary="showSummary"
+      // :rulesConfig="rulesConfig"
+
+
     },
   };
 </script>
