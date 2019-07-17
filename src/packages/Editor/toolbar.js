7dc363a9af2c486938d8393fbfaee2b55ba01dca
xilanhuadechuntian <Lensgcx@163.com>
2019-07-17 14:08:23 +0800

    Update toolbar.js

--- src/packages/Editor/toolbar.js
+++ src/packages/Editor/toolbar.js
@@ -4,7 +4,7 @@
 export default {
   simple: [],
 
-  normal: ['bold italic underline strikethrough alignleft aligncenter alignright outdent indent undo redo removeformat emoticons forecolor backcolor fullscreen'],
+  normal: ['bold italic underline strikethrough alignleft aligncenter alignright outdent indent undo redo removeformat emoticons forecolor backcolor fontsizeselect fontselect lineheight selectfullscreen'],
 
-  rich: ['searchreplace bold italic underline strikethrough alignleft aligncenter alignright outdent indent  blockquote undo redo removeformat subscript superscript code codesample', 'hr bullist numlist link image charmap preview anchor pagebreak insertdatetime media table emoticons forecolor backcolor fullscreen'],
+  rich: ['searchreplace bold italic underline strikethrough alignleft aligncenter alignright outdent indent  blockquote undo redo removeformat subscript superscript code codesample', 'hr bullist numlist link image charmap preview anchor pagebreak insertdatetime media table emoticons forecolor backcolor fontsizeselect fontselect lineheightselect fullscreen'],
 };
