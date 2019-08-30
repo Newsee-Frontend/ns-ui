6320cb2fdec02fa7bad53ebec5c049a964215a36
xilanhuadechuntian <Lensgcx@163.com>
2019-08-30 16:39:12 +0800

    [0.0.2-0] Upgrade to V-0.0.2-0

---
+++ version/saas/package_saas_19_8_30.js
@@ -0,0 +1,22 @@
+/**
+ * ns-saas-ui
+ * version："0.0.1"
+ */
+module.exports = [
+  'index.js',
+  'index.less',
+  'index.scss',
+  'style',
+  'mixins',
+  'utils',
+  '.DS_Store',
+  'Dropdown',
+  'Editor',
+  'Particles',
+  'Select',
+  'Only-MMDD-picker',
+  'Time-interval',
+  'Tree',
+  'VideoBackground',
+  'TestDemo',
+];
