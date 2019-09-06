e42f1307c36db3ec265326b7cc287e5100b6ed4f
xilanhuadechuntian <Lensgcx@163.com>
2019-09-06 10:39:20 +0800

    Update Input-tag.vue

--- artisan/src/pages/Form/Input-tag/Input-tag.vue
+++ artisan/src/pages/Form/Input-tag/Input-tag.vue
@@ -2,13 +2,46 @@
 <template>
   <div class="input-tag-wrapper">
     <demo-block>
-      <template slot="title">Input-tag</template>
+      <template slot="title">Input-tag 1</template>
       <template slot="content">
         <ns-input-tag
+          v-model="inputModel1"
+          size="normal"
           :options="optionsDataTemplate"
-          v-model="inputModel"
+          :prefixIcon="prefixIcon"
+
+        ></ns-input-tag>
+      </template>
+    </demo-block>
+
+    <demo-block>
+      <template slot="title">Input-tag 2</template>
+      <template slot="content">
+        <ns-input-tag
+          v-model="inputModel2"
+          size="medium"
+          :options="optionsDataTemplate"
+          :suffixIcon="suffixIcon"
+        ></ns-input-tag>
+      </template>
+    </demo-block>
+
+
+    <demo-block>
+      <template slot="title">Input-tag 2</template>
+      <template slot="content">
+        <ns-input-tag
+          size="large"
+          v-model="inputModel3"
+          :options="optionsDataTemplate"
           :prefixIcon="prefixIcon"
           :suffixIcon="suffixIcon"
+          @click="click"
+          @focus="onFocus"
+          @change="change"
+          @removeTag="removeTag"
+          @onClear="onClear"
+          @iconClick="iconClick"
         ></ns-input-tag>
       </template>
     </demo-block>
@@ -25,20 +58,39 @@
 
     data() {
       return {
-        inputModel: [],
+        inputModel1: [3, 4, 5],
+        inputModel2: [1, 2, 3, 4, 5, 7],
+        inputModel3: [6],
         prefixIcon: 'daohang', //输入框头部图标
         suffixIcon: 'gailan', //输入框尾部图标
+        //'mini', 'small', 'normal', 'medium', 'large'
       };
     },
 
     computed: {
       ...mapGetters(['optionsDataTemplate']),
     },
-    created() {
-
-    },
-    methods: {},
-    mounted() {
+    methods: {
+      click() {
+        console.log('click 事件');
+      },
+      onFocus() {
+        console.log('onFocus 事件');
+      },
+      change(val) {
+        console.log('change 事件');
+        console.log(val);
+      },
+      removeTag() {
+        console.log('removeTag 事件');
+      },
+      onClear() {
+        console.log('onClear 事件');
+      },
+      iconClick(type, val) {
+        alert(`图标-${type} 点击事件`);
+        console.log(val);
+      },
     },
   };
 </script>
