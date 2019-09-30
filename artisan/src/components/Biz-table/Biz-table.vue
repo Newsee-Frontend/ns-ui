170fab4ae59a5a533f89dac04d5f2ef2ef34e1f7
xilanhuadechuntian <Lensgcx@163.com>
2019-09-30 11:55:45 +0800

    Update Biz-table.vue

--- artisan/src/components/Biz-table/Biz-table.vue
+++ artisan/src/components/Biz-table/Biz-table.vue
@@ -99,7 +99,7 @@
           this.loadState.head = true;
         }
         else {
-          this.$store.dispatch('generateTableHead', { funcId: 'funcId' }).then(() => {
+          this.$store.dispatch('generateTableHead', { funcId: 'funcId' ,mockType: this.isFormTable ? 'form' : 'normal' }).then(() => {
             this.loadState.head = true;
           }).catch(() => {
             this.loadState.head = true;
