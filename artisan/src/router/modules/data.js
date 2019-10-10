51801bb166d6209337ba60dce43057dd76fa1e70
xilanhuadechuntian <Lensgcx@163.com>
2019-10-10 14:45:56 +0800

    Update data.js

--- artisan/src/router/modules/data.js
+++ artisan/src/router/modules/data.js
@@ -7,24 +7,6 @@ export const dataRouter = [
     name: '数据',
     children: [
       {
-        path: 'table',
-        component: () => import('../../pages/Data/Table/Table'),
-        meta: { auth: true, key: 'Table' },
-        name: 'Table 表格',
-      },
-      {
-        path: 'normal-table-layout',
-        component: () => import('../../pages/Data/Table/Table-layout-normal'),
-        meta: { auth: true, key: 'table-layout-normal' },
-        name: '常规表格标准页',
-      },
-      {
-        path: 'form-table-layout',
-        component: () => import('../../pages/Data/Table/Table-layout-form'),
-        meta: { auth: true, key: 'table-layout-form' },
-        name: '表单表格标准页',
-      },
-      {
         path: 'badge',
         component: () => import('../../pages/Data/Badge/Badge'),
         meta: { auth: true, key: 'Badge' },
