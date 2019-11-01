2cb0d617f21e50712a2b9a1d2167192f70dfc700
xilanhuadechuntian <Lensgcx@163.com>
2019-11-01 15:56:01 +0800

    Update index.js

--- artisan/src/UI/index.js
+++ artisan/src/UI/index.js
@@ -14,25 +14,21 @@ import '../assets/iconfont/iconfont';
 import '../icons';
 import '../components/index.js';
 
-
 //测试 - 单独引入各个组件模块
-// import './lib';
+import './lib';
 import Message from '../../../src/packages/Message/Message.js';
 Vue.prototype.$message = Message;
 
-
 // 测试 - 全局挂载全部组件
 
-import nsui from '../../../lib/ns-ui.min';
-import '../../../lib/index.scss';
+// import nsui from '../../../lib/ns-ui.min';
+// import '../../../lib/index.scss';
 
-console.log('全局挂载全部组件');
-console.log(nsui);
-Vue.use(nsui, {
-  prefix: 'ns', //stage name
-  tie: '-',
-  log: false, //show log information
-  type: 'globle',
-});
-
-
+// console.log('全局挂载全部组件');
+// console.log(nsui);
+// Vue.use(nsui, {
+//   prefix: 'ns', //stage name
+//   tie: '-',
+//   log: false, //show log information
+//   type: 'globle',
+// });
