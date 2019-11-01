ba26ee66722ea2481f20dfd4671aba96a9d5e166
xilanhuadechuntian <Lensgcx@163.com>
2019-11-01 15:55:22 +0800

    Update fetch.js

--- artisan/src/fetch/fetch.js
+++ artisan/src/fetch/fetch.js
@@ -24,7 +24,7 @@ service.interceptors.request.use(
   },
   error => {
     Promise.reject(error);
-  },
+  }
 );
 
 service.interceptors.response.use(
@@ -41,7 +41,7 @@ service.interceptors.response.use(
     Message({ message: error.resultMsg, type: 'error', duration: 2000 }); //throw message error
     let errorInfo = error.data.error ? error.data.error.message : error.data;
     return Promise.reject(errorInfo);
-  },
+  }
 );
 
 export default service;
