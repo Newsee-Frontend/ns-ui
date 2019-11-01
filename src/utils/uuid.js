91c1ea399d8f979d9e4f24e64e1d56aea4f78d71
xilanhuadechuntian <Lensgcx@163.com>
2019-11-01 15:57:31 +0800

    Update uuid.js

--- src/utils/uuid.js
+++ src/utils/uuid.js
@@ -1,5 +1,7 @@
 export const uuid = length => {
-  return Number(Math.random().toString().substr(3, length) + Date.now()).toString(36);
+  return Number(
+    Math.random()
+      .toString()
+      .substr(3, length) + Date.now()
+  ).toString(36);
 };
-
-
