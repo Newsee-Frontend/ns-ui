dbf37505591d3084b700a804d913b7065758f724
xilanhuadechuntian <Lensgcx@163.com>
2019-11-01 15:58:00 +0800

    Update cell-fifter.js

--- artisan/src/components/Biz-table/cell-fifter.js
+++ artisan/src/components/Biz-table/cell-fifter.js
@@ -8,14 +8,12 @@ export default (val, key) => {
   if (key === 'sex') {
     if (val === '1') {
       return '男';
-    }
-    else if (val === '2') {
+    } else if (val === '2') {
       return '女';
-    }
-    else {
+    } else {
       return val;
     }
   }
 
   return val;
-}
+};
