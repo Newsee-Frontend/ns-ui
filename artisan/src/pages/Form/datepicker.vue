2a7294fd248367345570a8c9538cd1a73739809f
zhouxia <zhouxia@servyou.com.cn>
2019-11-06 13:50:42 +0800

    修改时间样式

--- artisan/src/pages/Form/datepicker.vue
+++ artisan/src/pages/Form/datepicker.vue
@@ -10,9 +10,6 @@
         <div class="gap">
           <ns-date-picker
             v-model="date"
-            size="medium"
-            :width="width"
-            :height="height"
             :readonly="readonly"
             :disabled="disabled"
             :editable="editable"
