4d2505bd9c15c47fe987ecaf8d8f1aaa0ae2e60a
xilanhuadechuntian <Lensgcx@163.com>
2019-11-14 16:21:47 +0800

    del

--- src/packages/Table/mixins/column.js
+++
@@ -1,9 +0,0 @@
-export default {
-  data() {
-    return {
-      normalColInclude: ['text', 'number', 'date', 'select'],
-      specialColInclude: ['index', 'selection', 'radio'],
-      actionColInclude: ['action', 'add-row'],
-    };
-  },
-};
