509f582b589227802dd5885985a8b85eafd97ead
xilanhuadechuntian <Lensgcx@163.com>
2019-11-14 16:24:33 +0800

    Create simple-column.js

---
+++ artisan/src/pages/Table/New-table-v4/hugeDataTable/simple-column.js
@@ -0,0 +1,109 @@
+export default [
+  {
+    'resourcecolumnName': '项目名称',//label
+    'resourcecolumnNameEn': 'taskName',//model-key
+    'resourcecolumnCode': 'taskName',//model-key
+    'resourcecolumnXtype': 'text',//筛选列 类型
+    'resourcecolumnWidth': '150',//列的宽度
+    'resourcecolumnOrder': '4',//列的序号
+    'resourcecolumnHidden': '1', //是否隐藏
+    'resourcecolumnAlign': 'left',//对齐
+    'eidtConfig': null
+  },
+  {
+    'resourcecolumnName': '拥有人',//label
+    'resourcecolumnCode': 'ownerName',//model-key
+    'resourcecolumnXtype': 'text',//筛选列 类型
+    'resourcecolumnWidth': '200',//列的宽度
+    'resourcecolumnOrder': '4',//列的序号
+    'resourcecolumnHidden': '0', //是否隐藏
+    'resourcecolumnAlign': 'left',//对齐
+    'eidtConfig': null
+  },
+  {
+    'resourcecolumnName': '年龄',//label
+    'resourcecolumnCode': 'age',//model-key
+    'resourcecolumnXtype': 'text',//筛选列 类型
+    'resourcecolumnWidth': '100',//列的宽度
+    'resourcecolumnOrder': '5',//列的序号
+    'resourcecolumnHidden': '0', //是否隐藏
+    'resourcecolumnAlign': 'left',//对齐
+    'eidtConfig': null
+  },
+  {
+    'resourcecolumnName': '成立日期',//label
+    'resourcecolumnCode': 'createDate',//model-key
+    'resourcecolumnXtype': 'text',//筛选列 类型
+    'resourcecolumnWidth': '200',//列的宽度
+    'resourcecolumnOrder': '5',//列的序号
+    'resourcecolumnHidden': '0', //是否隐藏
+    'resourcecolumnAlign': 'left',//对齐
+    'eidtConfig': null
+  },
+  {
+    'resourcecolumnName': '等级调整',//label
+    'resourcecolumnNameEn': 'level',//model-key
+    'resourcecolumnCode': 'level',
+    'resourcecolumnXtype': 'text',//筛选列 类型
+    'resourcecolumnWidth': '150',//列的宽度
+    'resourcecolumnOrder': '10',//列的序号
+    'resourcecolumnHidden': '0', //是否隐藏
+    'resourcecolumnAlign': 'left',//对齐
+    'eidtConfig': null
+  },
+  {
+    'resourcecolumnName': '是否审核',//label
+    'resourcecolumnNameEn': 'isChecked',//model-key
+    'resourcecolumnCode': 'isChecked',//model-key
+    'resourcecolumnXtype': 'text',//筛选列 类型
+    'resourcecolumnWidth': '120',//列的宽度
+    'resourcecolumnOrder': '4',//列的序号
+    'resourcecolumnHidden': '0', //是否隐藏
+    'resourcecolumnAlign': 'left',//对齐
+    'eidtConfig': null
+  },
+
+  {
+    'resourcecolumnName': '是否锁定（单选）',//label
+    'resourcecolumnNameEn': 'isLocked',//model-key
+    'resourcecolumnCode': 'isLocked',
+    'resourcecolumnXtype': 'text',//筛选列 类型
+    'resourcecolumnWidth': '150',//列的宽度
+    'resourcecolumnOrder': '4',//列的序号
+    'resourcecolumnHidden': '0', //是否隐藏
+    'resourcecolumnAlign': 'left',//对齐
+    'eidtConfig': null
+  },
+  {
+    'resourcecolumnName': '邮箱',//label
+    'resourcecolumnCode': 'email',//model-key
+    'resourcecolumnXtype': 'text',//筛选列 类型
+    'resourcecolumnWidth': '200',//列的宽度
+    'resourcecolumnOrder': '4',//列的序号
+    'resourcecolumnHidden': '0', //是否隐藏
+    'resourcecolumnAlign': 'left',//对齐
+    'eidtConfig': null
+  },
+
+  {
+    'resourcecolumnName': '涉猎领域（多选）',//label
+    'resourcecolumnNameEn': 'fieldArea',//model-key
+    'resourcecolumnCode': 'fieldArea',
+    'resourcecolumnXtype': 'text',//筛选列 类型
+    'resourcecolumnWidth': '600',//列的宽度
+    'resourcecolumnOrder': '5',//列的序号
+    'resourcecolumnHidden': '0', //是否隐藏
+    'resourcecolumnAlign': 'left',//对齐
+    'eidtConfig': null
+  },
+];
+
+
+// { type: 'index', width: 60, t: 1 },
+// { type: 'radio', width: 50, t: 1 },
+// { type: 'selection', width: 50, t: 1 },
+// { field: 'name', width: 100, title: 'Name', t: 2 },
+// { field: 'nickname', width: 200, title: 'Nickname', editRender: { name: 'input' }, t: 3 },
+// { field: 'age', width: 100, title: 'Age', t: 2 },
+// { field: 'role', width: 100, title: 'Role', editRender: { name: 'input' }, t: 3 },
+// { field: 'describe', width: 800, title: 'Describe', showOverflow: true, editRender: { name: 'input' }, t: 3 },
