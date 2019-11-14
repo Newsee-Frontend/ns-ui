53c56355fb5cd5739c449fd41a7a5da2faa91872
xilanhuadechuntian <Lensgcx@163.com>
2019-11-14 16:22:22 +0800

    Create Table-v4.js

---
+++ src/packages/Table-v4/Table-v4.js
@@ -0,0 +1,14 @@
+/*
+ * table component V4.0
+ * you can use this component in any application.
+ * created: 2019/11/11.
+ * author: Broccoli spring( 高仓雄 - gcx )
+ * copyright (c) 2019 Broccoli spring( gcx )
+ */
+
+import './plugins/utils';
+import './plugins/loader';
+
+import Main from './main.js';
+
+export default Main;
