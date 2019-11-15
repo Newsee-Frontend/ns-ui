551814515daf2fe3c88dc7731a2bfc0771ae3064
xilanhuadechuntian <Lensgcx@163.com>
2019-11-15 16:27:20 +0800

    Create index.js

---
+++ artisan/src/UI/NS-UI/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+
+//测试 - 单独引入各个组件模块
+// import './lib';
+
+
+//测试 - 全局挂载全部组件
+import nsui from '../../../../lib/ns-ui';
+import '../../../../lib/index.scss';
+
+console.log('全局挂载全部组件');
+console.log(nsui);
+Vue.use(nsui, {
+  prefix: 'ns', //stage name
+  tie: '-',
+  log: false, //show log information
+  type: 'globle',
+});
+
+
+import Message from '../../../../src/packages/Message/Message.js';
+
+Vue.prototype.$message = Message;
