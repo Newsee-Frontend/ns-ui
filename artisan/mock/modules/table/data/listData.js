20e4bdd8542e8a15f6e42bcf842aad3603b081bd
xilanhuadechuntian <Lensgcx@163.com>
2019-11-18 17:24:05 +0800

    Update listData.js

--- artisan/mock/modules/table/data/listData.js
+++ artisan/mock/modules/table/data/listData.js
@@ -1,5 +1,59 @@
 const Mock = require('mockjs');
 
+/**
+ * new from table data
+ * @param pageSize  - 每页的表格数据量
+ */
+export const newFormTableData = pageSize => {
+  console.log('new from table data');
+  console.log(pageSize);
+  return Array.from({ length: pageSize }, k => k).map((item, index) => {
+    return Mock.mock({
+      taskName: function(n) {
+        const task = n.context.currentContext.ownerName;
+        const createDate = n.context.currentContext.createDate;
+        return task + '的项目-' + createDate;
+      }, //项目名称
+      ownerName: '@cname',
+      age: Mock.Random.float(20, 60, 0, 0), //年龄
+      sex: {
+        picked: { value: Mock.Random.float(0, 2, 0, 0) },
+        options: [{ label: '男', value: 0 }, { label: '女', value: 1 }, { label: '保密', value: 2 }],
+      },//性别
+      isChecked: {
+        picked: { value: '' },
+        options: [],
+      },//是否审核
+      checkedType: {
+        picked: { value: '' },
+        options: [],
+      },//审核类型
+      // createDate: '@datetime', //成立日期
+      createDate: Mock.Random.date('yyyy-MM-dd hh:mm:ss'),//成立日期
+      endDate: Mock.Random.date('yyyy-MM-dd'),//结束 - 日期
+      endTime: Mock.Random.date('hh:mm:ss'),//结束 -  时间点
+      level: Mock.Random.float(1, 100, 0, 1), //等级调整
+      isLocked: {
+        picked: { value: 1 },
+        options: [{ 'label': '是', 'value': 1 }, { 'label': '否', 'value': 0 }],
+      },//是否锁定（单选）
+      fieldArea: {
+        picked: { value: [1, 2, 4, 5] },
+        options: [
+          { label: '互联网', value: 1 },
+          { label: '电子芯片', value: 2 },
+          { label: '传统制造', value: 3 },
+          { label: '物联网', value: 4 },
+          { label: '广告服务', value: 5 },
+        ],
+      },//涉猎领域（多选）
+      describe: Mock.Random.csentence(), //备注
+      email: Mock.Random.email(),
+    });
+  });
+};
+
+
 export const formTableData = total =>
   Array.from({ length: total }, k => k).map((item, index) => {
     return Mock.mock({
