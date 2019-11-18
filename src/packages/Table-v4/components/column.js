13cbc17072b4ec95346576ea3630201bd806fe2a
xilanhuadechuntian <Lensgcx@163.com>
2019-11-18 17:24:13 +0800

    Update column.js

--- src/packages/Table-v4/components/column.js
+++ src/packages/Table-v4/components/column.js
@@ -31,15 +31,15 @@ export default {
   },
   computed: {
     columnType() {
-      return this.column[this.headRefer['xtype']];
+      return this.column.type;
     },
     modelCode() {
-      return this.column[this.headRefer['model-key']];
+      return this.column.field;
     },
 
     //表单单元格配置信息
     formConfig() {
-      return this.column[this.headRefer['cell-Config']];
+      return this.column['cell-Config'];
     },
     //表单单元格类型
     formType() {
@@ -68,10 +68,43 @@ export default {
   render(h) {
     const injection = {};
 
-    //常规列( 常规列 / 链接列 / 表单列 )
-    if (this.normalColInclude.indexOf(this.columnType) > -1) {
+    //操作列
+    if (this.actionColInclude.indexOf(this.columnType) > -1) {
       injection.props = {
-        field: this.column[this.headRefer['model-key']],
+        'cell-render': {
+          name: 'ns-table-action-cell',
+          props: {
+            column: this.column,
+            scopeRefer: this.scopeRefer,
+          },
+          events: {
+            click: this.tableAction,
+          },
+        },
+      };
+    }
+    //设置列
+    if (this.settingColInclude.indexOf(this.columnType) > -1) {
+      injection.scopedSlots = {
+        default: () => {
+          return [];
+        },
+        header: scope => {
+          return (
+            <action-drop
+              customColumns={this.customColumns}
+              on-sync-column-render={this.syncColRender}
+              on-column-setting-submit={this.columnSettingSubmit}
+            />
+          );
+        },
+      };
+    }
+
+    //常规列( 常规列 / 链接列 / 表单列 )
+    else if (this.normalColInclude.indexOf(this.columnType) > -1) {
+      injection.props = {
+        field: this.column.field,
       };
 
       if (this.isFormRender) {
@@ -102,56 +135,13 @@ export default {
       }
     }
 
-    //操作列
-    else if (this.actionColInclude.indexOf(this.columnType) > -1) {
-      injection.props = {
-        'cell-render': {
-          name: 'ns-table-action-cell',
-          props: {
-            column: this.column,
-            scopeRefer: this.scopeRefer,
-          },
-          events: {
-            click: this.tableAction,
-          },
-        },
-      };
-    }
-
-    //设置列
-    else if (this.settingColInclude.indexOf(this.columnType) > -1) {
-      injection.scopedSlots = {
-        default: () => {
-          return [];
-        },
-        header: scope => {
-          return (
-            <action-drop
-              customColumns={this.customColumns}
-              on-sync-column-render={this.syncColRender}
-              on-column-setting-submit={this.columnSettingSubmit}
-            />
-          );
-        },
-      };
-    } else {
-    }
-
-    console.log('当前字段');
-    console.log(this.column[this.headRefer['model-key']]);
-    console.log('当前列类型');
-    console.log(this.columnType);
-
-    if (this.column[this.headRefer['model-key']] === 'organizationShortName') {
-      console.log('问题列');
-    }
 
     const general = {
       props: {
         type: this.columnType,
-        title: this.column[this.headRefer['label']],
-        align: this.column[this.headRefer['align']],
-        fixed: this.column[this.headRefer['fixed']],
+        title: this.column.title,
+        align: this.column.align,
+        fixed: this.column.fixed,
         'header-class-name': ({ column }) => {
           return `column-${column.property || column.type}`;
         },
@@ -164,7 +154,7 @@ export default {
      */
     const wk = this.specialColumns.indexOf(this.columnType) > -1 ? 'width' : 'min-width';
 
-    general.props[wk] = this.column[this.headRefer['width']];
+    general.props[wk] = this.column.width;
 
     return h(`vxe-table-column`, deepObjectMerge(general, injection));
   },
