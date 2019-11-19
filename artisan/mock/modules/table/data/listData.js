a27603c818209f206e1a5ca8c07a7a992c9a7463
xilanhuadechuntian <Lensgcx@163.com>
2019-11-19 16:19:31 +0800

    Update listData.js

--- artisan/mock/modules/table/data/listData.js
+++ artisan/mock/modules/table/data/listData.js
@@ -1,12 +1,10 @@
 const Mock = require('mockjs');
 
 /**
- * new from table data
+ * new from table data - 表单表格模拟数据
  * @param pageSize  - 每页的表格数据量
  */
-export const newFormTableData = pageSize => {
-  console.log('new from table data');
-  console.log(pageSize);
+export const formTableData = pageSize => {
   return Array.from({ length: pageSize }, k => k).map((item, index) => {
     return Mock.mock({
       taskName: function(n) {
@@ -53,56 +51,82 @@ export const newFormTableData = pageSize => {
   });
 };
 
-
-export const formTableData = total =>
-  Array.from({ length: total }, k => k).map((item, index) => {
+/**
+ * 大数据表格模拟数据
+ * @param pageSize
+ * @returns {any[]}
+ */
+export const hugeDataTableData = (pageSize) => {
+  const random = Math.ceil(Math.random() * 10);
+  return Array.from({ length: pageSize }, k => k).map((item, index) => {
     return Mock.mock({
       taskName: function(n) {
         const task = n.context.currentContext.ownerName;
-        const createDate = n.context.currentContext.createDate;
-        return task + '的项目-' + createDate;
+        return task + '的项目-';
       }, //项目名称
-      isChecked: {
-        picked: { value: 1 },
-        options: [{ label: '已审核', value: 1 }, { label: '未审核', value: 0 }],
-      }, //是否审核
+      ownerName: `gcx-${random}`,
+      age: random, //年龄
       createDate: '@datetime', //成立日期
-      ownerName: '@cname', //使用人
-      level: Mock.Random.float(1, 100, 0, 1), //等级调整
-      field: {
-        picked: { value: [1] },
-        options: [
-          { label: '互联网', value: 1 },
-          { label: '电子芯片', value: 2 },
-          { label: '传统制造', value: 3 },
-          { label: '物联网', value: 4 },
-          { label: '广告服务', value: 5 },
-        ],
-      }, //涉猎领域（多选）
-      isLocked: {
-        picked: { value: 1 },
-        options: [{ label: '是', value: 1 }, { label: '否', value: 0 }],
-      }, //是否锁定（单选）
-      remark: '', //备注 Mock.Random.county(true)
+      level: random, //等级调整
+      isChecked: '已审核',//是否审核
+      isLocked: '是',//是否锁定（单选）
+      fieldArea: '互联网',//涉猎领域（多选）
+      email: 'Lensgcx@163.com',
     });
   });
+};
 
-export const normalTableData = total =>
-  Array.from({ length: total }, k => k).map((item, index) => {
-    return Mock.mock({
-      taskName: function(n) {
-        const task = n.context.currentContext.ownerName;
-        const createDate = n.context.currentContext.createDate;
-        return task + '的项目-' + createDate;
-      }, //项目名称
-      createDate: '@datetime', //成立日期
-      ownerName: '@cname', //使用人
-      level: Mock.Random.float(1, 100, 0, 1), //等级调整
-      id: Mock.Random.id(), //id
-      address: Mock.Random.city(true), //地址
-      zip: Mock.Random.zip(), //邮政编码
-      ip: Mock.Random.ip(), //ip 地址
-      email: Mock.Random.email(), //邮箱 地址
-      remark: Mock.Random.csentence(), //备注
-    });
-  });
+
+// export const formTableData = total =>
+//   Array.from({ length: total }, k => k).map((item, index) => {
+//     return Mock.mock({
+//       taskName: function(n) {
+//         const task = n.context.currentContext.ownerName;
+//         const createDate = n.context.currentContext.createDate;
+//         return task + '的项目-' + createDate;
+//       }, //项目名称
+//       isChecked: {
+//         picked: { value: 1 },
+//         options: [{ label: '已审核', value: 1 }, { label: '未审核', value: 0 }],
+//       }, //是否审核
+//       createDate: '@datetime', //成立日期
+//       ownerName: '@cname', //使用人
+//       level: Mock.Random.float(1, 100, 0, 1), //等级调整
+//       field: {
+//         picked: { value: [1] },
+//         options: [
+//           { label: '互联网', value: 1 },
+//           { label: '电子芯片', value: 2 },
+//           { label: '传统制造', value: 3 },
+//           { label: '物联网', value: 4 },
+//           { label: '广告服务', value: 5 },
+//         ],
+//       }, //涉猎领域（多选）
+//       isLocked: {
+//         picked: { value: 1 },
+//         options: [{ label: '是', value: 1 }, { label: '否', value: 0 }],
+//       }, //是否锁定（单选）
+//       remark: '', //备注 Mock.Random.county(true)
+//     });
+//   });
+
+
+// export const normalTableData = total =>
+//   Array.from({ length: total }, k => k).map((item, index) => {
+//     return Mock.mock({
+//       taskName: function(n) {
+//         const task = n.context.currentContext.ownerName;
+//         const createDate = n.context.currentContext.createDate;
+//         return task + '的项目-' + createDate;
+//       }, //项目名称
+//       createDate: '@datetime', //成立日期
+//       ownerName: '@cname', //使用人
+//       level: Mock.Random.float(1, 100, 0, 1), //等级调整
+//       id: Mock.Random.id(), //id
+//       address: Mock.Random.city(true), //地址
+//       zip: Mock.Random.zip(), //邮政编码
+//       ip: Mock.Random.ip(), //ip 地址
+//       email: Mock.Random.email(), //邮箱 地址
+//       remark: Mock.Random.csentence(), //备注
+//     });
+//   });
