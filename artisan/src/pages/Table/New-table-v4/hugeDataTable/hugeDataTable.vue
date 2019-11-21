c0bc8d0dd9ea196e6a27511f14b73ded788159e1
xilanhuadechuntian <Lensgcx@163.com>
2019-11-21 15:33:23 +0800

    Update hugeDataTable.vue

--- artisan/src/pages/Table/New-table-v4/hugeDataTable/hugeDataTable.vue
+++ artisan/src/pages/Table/New-table-v4/hugeDataTable/hugeDataTable.vue
@@ -23,6 +23,7 @@
         <biz-table-v4
           ref="hugeDataTable"
           :loading="loading"
+          :data="tableData.list"
           :total="total"
           isHugeData
           :searchConditions="searchConditions"
@@ -38,22 +39,20 @@
   import bizTableV4 from '../../../../components/Biz-table/Biz-table-v4/Biz-table-v4';
   import { tableDataService } from '../../../../service/Table/index';
 
-
   export default {
     name: 'new-hugeDataTable',
     components: { bizTableV4 },
     data() {
       return {
         loading: false,
-        data: {},
-
-        roleBtns: [],
+        tableData: {},
+        gridBtns: [],
         searchConditions: {
           companyId: '', //公司id
           departmentId: '', //部门id
           filterList: [], //条件
           pageNum: 1, //当前页数
-          pageSize: 10, //每页显示条目个数
+          pageSize: 1000, //每页显示条目个数
           orderBy: '', //排序：升序还是降序
           orderFieldName: '', //排序：字段名
           mainSearch: '', //输入框值
@@ -69,66 +68,59 @@
     },
     computed: {
       total() {
-        return this.data.total;
+        return this.tableData.total;
       },
     },
     methods: {
-      getTableData(type = 'reloadData') {
+      getTableData() {
         this.loading = true;
 
         tableDataService({ query: this.searchConditions, funcId: 'funcId' }).then(res => {
-          this.data = res.resultData || {};
+          this.tableData = res.resultData || {};
 
           console.log('请求到的表格数据：');
-          console.log(this.data);
+          console.log(this.tableData);
 
-          this.data.list.forEach(item => {
-            item.fnsclick = this.roleBtns;
-          });
+          this.tableLoader(this.gridBtns);
 
-
-          console.log(3333333333333);
-          console.log(this.$refs['hugeDataTable']);
-          console.log(this.$refs['hugeDataTable'].reloadData);
-          if (type === 'reloadData') {
-            this.$refs['hugeDataTable'].reloadData(this.data.list).then(() => {
-              this.loading = false;
-            }).catch(err => {
-              console.log('catch-catch');
-              console.log(err);
-              this.loading = false;
-            });
-          }
-          else {
-            this.$refs['hugeDataTable'].loadData(this.data.list).then(() => {
-              this.loading = false;
-            }).catch(err => {
-              console.log('catch-catch');
-              console.log(err);
-              this.loading = false;
-            });
-
-          }
+          this.loading = false;
 
         }).catch(() => {
           this.loading = false;
+        });
+      },
+      /**
+       * 表格加载处理
+       * 可在其中做一些业务操作，如：
+       *  1、增加权限按钮
+       *  2、过滤数据
+       *  3、其他异步请求
+       * @param gridBtns  - 参数，表格的权限按钮列表数据
+       */
+      tableLoader(gridBtns = []) {
+        //当表数据和表格权限按钮数据同时获取到位后，才进行后续操作
+        if (gridBtns.length === 0) return;
+        if (!this.tableData.list) return;
+
+        //为表格增加权限按钮
+        this.tableData.list.forEach(item => {
+          this.$set(item, 'fnsclick', gridBtns);
         });
       },
     },
 
     created() {
       setTimeout(() => {
-        this.roleBtns = [
+        this.gridBtns = [
           { label: '新增授权人', value: 'addshouquanren' },
           { label: '编辑', value: 'gridEditBtn' },
           { label: '删除', value: 'gridRemoveBtn' },
         ];
-        console.log(4444444444);
-        this.data.list.forEach(item => {
-          item.fnsclick = this.roleBtns;
-        });
-        this.$refs['hugeDataTable'].reloadData(this.data.list);
-      }, 2000);
+
+        this.tableLoader(this.gridBtns);
+
+      }, 5000);
+
     },
     mounted() {
       this.getTableData('reloadData');
