8b97bf40c74e90f5c4cd251bd3721cb00d0ffecd
xilanhuadechuntian <Lensgcx@163.com>
2019-11-25 17:09:49 +0800

    Update summary-drop.js

--- src/packages/Table-v4/components/summary-drop.js
+++ src/packages/Table-v4/components/summary-drop.js
@@ -3,25 +3,32 @@ export default {
   data() {
     return {
       dropTit: '合计',
-      summaryOption: [{ command: 'current', tit: '分页', menutit: '分页合计' }, { command: 'total', tit: '全部', menutit: '全部合计' }],
+      summaryOption: [
+        { command: 'current', tit: '分页', menutit: '分页合计' },
+        { command: 'total', tit: '全部', menutit: '全部合计' },
+      ],
     };
   },
   render(h) {
-    return <div class="summary-area">
-      <el-dropdown trigger="click" on-command={this.handleCommand}>
-              <span class="el-dropdown-link">
-                {this.dropTit}
-                <i class="el-icon-arrow-down el-icon--right"/>
-              </span>
-        <el-dropdown-menu slot="dropdown" class="summary-menu">
-          {
-            this.summaryOption.map((item, index) => {
-              return <el-dropdown-item key={index} command={item.command}>{item.menutit}</el-dropdown-item>;
-            })
-          }
-        </el-dropdown-menu>
-      </el-dropdown>
-    </div>;
+    return (
+      <div class="summary-area">
+        <el-dropdown trigger="click" on-command={this.handleCommand}>
+          <span class="el-dropdown-link">
+            {this.dropTit}
+            <i class="el-icon-arrow-down el-icon--right" />
+          </span>
+          <el-dropdown-menu slot="dropdown" class="summary-menu">
+            {this.summaryOption.map((item, index) => {
+              return (
+                <el-dropdown-item key={index} command={item.command}>
+                  {item.menutit}
+                </el-dropdown-item>
+              );
+            })}
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    );
   },
   methods: {
     /**
