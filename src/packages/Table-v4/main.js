ab3099c3a7a4f05301246daa403349b17c26ca13
xilanhuadechuntian <Lensgcx@163.com>
2019-11-25 17:04:33 +0800

    Update main.js

--- src/packages/Table-v4/main.js
+++ src/packages/Table-v4/main.js
@@ -1,16 +1,22 @@
 import create from '../../create/create';
+
 import selection from './mixins/selection';
 import reRender from './mixins/reRender';
 import validate from './mixins/validate';
+import columntype from './mixins/columntype';
 import namefactory from './mixins/namefactory';
+import errorType from './mixins/errorType';
+
 import columnRender from './components/column';
+import identifier from './components/identifier';
+import summaryDrop from './components/summary-drop';
 
 import img_null from '../../assets/null.jpg';
 
 export default create({
   name: 'table',
-  components: { columnRender },
-  mixins: [selection, reRender, validate, namefactory],
+  components: { columnRender, summaryDrop, identifier },
+  mixins: [selection, reRender, validate, namefactory, columntype, errorType],
   props: {
     loading: { type: Boolean, default: true },
     keyRefer: { type: Object },
@@ -43,6 +49,12 @@ export default create({
       customColumns: [],
     };
   },
+  computed: {
+    isMainReady() {
+      //Header data must exist and loading status must be false
+      return (this.head && this.head.length);
+    },
+  },
   watch: {
     head: {
       handler: function(val) {
@@ -52,14 +64,6 @@ export default create({
             visible: !item.hidden,
           };
         });
-
-        // console.log('表格组件 - 表头数据发生变化');
-        // console.log('表格组件 - 表头数据发生变化');
-        // console.log('表格组件 - 表头数据发生变化');
-        // console.log(val);
-        // console.log(this.customColumns);
-        // console.log('表格组件 - 表头数据发生变化');
-        // console.log('表格组件 - 表头数据发生变化');
 
         if (this.$refs['main-table']) {
           this.$refs['main-table'].refreshColumn();
@@ -79,6 +83,7 @@ export default create({
       size: 'small',
       height: this.height,
       resizable: true,
+      'highlight-hover-row': true,
       'show-overflow': true,
       'show-header-overflow': true,
       'edit-config': this.editConfig,
@@ -98,74 +103,86 @@ export default create({
       props.data = this.data;
     }
 
-    // console.log(111111111111);
-    // console.log(111111111111);
-    // console.log(this.head);
-    // console.log(this.customColumns);
-    // console.log(111111111111);
-    // console.log(111111111111);
 
-    // console.log('表格渲染的数据如下：');
-    // console.log('表格渲染的数据如下：');
-    // console.log(this.data);
-    // console.log(this.head);
-    // console.log('==================');
-
-    return this.head && this.head.length
-      ? h(
-          `vxe-table`,
-          {
-            ref: 'main-table',
-            class: this.recls(),
-            props: props,
-            on: {
-              'edit-actived': this.editActived,
-              'select-change': this.selectChange,
-              'select-all': this.selectAll,
-              'update:customs': value => {
-                this.customColumns = value;
-                this.$nextTick(() => {
-                  this.$refs['main-table'].refreshColumn();
-                });
+    return <div class={this.recls()} style={`height: ${this.height}px`}>
+      {
+        this.isMainReady ?
+          h(
+            `vxe-table`,
+            {
+              ref: 'main-table',
+              props: props,
+              on: {
+                'edit-actived': this.editActived,
+                'select-change': this.selectChange,
+                'select-all': this.selectAll,
+                'update:customs': value => {
+                  this.customColumns = value;
+                  this.$nextTick(() => {
+                    this.$refs['main-table'].refreshColumn();
+                  });
+                },
               },
-            },
-            scopedSlots: {
-              empty: scope => {
-                return (
-                  <span class={'error-prompt'}>
-                    <img class={'errorImg'} src={img_null} />
+              scopedSlots: {
+                empty: scope => {
+                  return (
+                    <span class={'error-prompt'}>
+                    <img class={'errorImg'} src={img_null}/>
                     <p> 抱歉, 没有搜索到你要的结果 </p>
                   </span>
-                );
+                  );
+                },
               },
             },
-          },
-          [
-            this.head.map((item, $index) => {
-              return h(`column-render`, {
-                key: `column-render-${$index}`,
-                props: {
-                  column: item,
-                  columns: this.head,
-                  keyRefer: this.keyRefer,
-                  customColumns: this.customColumns,
-                },
-                on: {
-                  ...this.$listeners,
-                  'sync-column-render': data => {
-                    // console.log(data.customColumns);
-
-                    const target = this.$refs['main-table'];
-                    ['change', 'lock'].indexOf(data.event) > -1
-                      ? target.refreshColumn()
-                      : target.reloadColumn(data.customColumns);
+            [
+              this.head.map((item, $index) => {
+                return h(`column-render`, {
+                  key: `column-render-${$index}`,
+                  props: {
+                    column: item,
+                    columns: this.head,
+                    keyRefer: this.keyRefer,
+                    customColumns: this.customColumns,
                   },
-                },
-              });
-            }),
-          ]
-        )
-      : null;
+                  on: {
+                    ...this.$listeners,
+                    'sync-column-render': data => {
+                      // console.log(data.customColumns);
+
+                      const target = this.$refs['main-table'];
+                      ['change', 'lock'].indexOf(data.event) > -1
+                        ? target.refreshColumn()
+                        : target.reloadColumn(data.customColumns);
+                    },
+                  },
+                });
+              }),
+            ],
+          )
+          : null
+      }
+      {
+        this.showFooter ?
+          h('summary-drop', {
+            on: {
+              ...this.$listeners,
+            },
+          })
+          : null
+      }
+      {
+        h('identifier', {
+          class: this.recls('mask'),
+          props: {
+            loading: this.loading,
+            errorType: this.errorType,
+          },
+          on: {
+            ...this.$listeners,
+          },
+        })
+      }
+    </div>;
   },
   methods: {
     fullValidate(cb) {
@@ -209,6 +226,8 @@ export default create({
       this.$refs['main-table'].clearActived();
     },
   },
-  created() {},
-  mounted() {},
+  created() {
+  },
+  mounted() {
+  },
 });
