8c259051d4646d11ea1bc8e50022255455c1d787
xilanhuadechuntian <Lensgcx@163.com>
2019-11-28 11:59:40 +0800

    Update select.js

--- src/packages/Table-v4/renderer/select.js
+++ src/packages/Table-v4/renderer/select.js
@@ -9,26 +9,41 @@ export default {
       let { events } = editRender;
       return [
         <Select
-          value={row[modelCode].picked.value}
+          value={row[modelCode]}
           onInput={e => {
-            row[modelCode].picked.value = e;
+            row[modelCode] = e;
             this.$emit('input', e);
           }}
-          options={row[modelCode].options}
+          options={formConfig.options}
           width={'100%'}
           editable={false}
           placeholder={formConfig.placeHolder}
           disabled={formConfig.disabled}
           clearable
+          multiple={formConfig.multiple}
+          multiple-limit={formConfig.max}
           on-change={() => events.change({ row, column, rowIndex, columnIndex })}
         />,
       ];
     },
 
     renderCell(h, editRender, { row }) {
-      const { modelCode } = editRender.props;
-      let data = row[modelCode];
-      return getLabelByValue(data.picked.value, data.options);
+      const { modelCode, formConfig, column } = editRender.props;
+
+      const model = row[modelCode];
+      const options = column['cell-Config'].options;
+
+      if (formConfig.multiple) {
+        if (!Array.isArray(model)) {
+          return [];
+        }
+
+        return model.map(value => {
+          return <span>{`${getLabelByValue(value, options)}  `}</span>;
+        });
+      } else {
+        return getLabelByValue(model, options);
+      }
     },
   },
 };
