0929da1f254a23cf40f8b98c61a79cd09c385cff
xilanhuadechuntian <Lensgcx@163.com>
2019-11-28 16:51:27 +0800

    Update loader.js

--- artisan/src/UI/Table/loader.js
+++ artisan/src/UI/Table/loader.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 
 import VXETable from 'vxe-table';
 import 'vxe-table/lib/index.css';
+import '../../style/cover/vxe-table/index.scss';
 
 import VXETablePluginElement from 'vxe-table-plugin-element';
 
