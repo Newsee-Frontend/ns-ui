f9c11422f0ab85e93b41447efa03d43fe83d4d4d
xilanhuadechuntian <Lensgcx@163.com>
2019-12-05 10:40:34 +0800

    Update dataRender.js

--- artisan/src/components/Biz-table/Biz-table-v4/mixins/dataRender.js
+++ artisan/src/components/Biz-table/Biz-table-v4/mixins/dataRender.js
@@ -1,3 +1,7 @@
+/**
+ * 关于业务表格表数据的操作
+ * 在大数据表格中，监听表头 / 表数据 获取 - 是否准备完毕，进而加载表数据
+ */
 export default {
   data() {
     return {
@@ -5,25 +9,26 @@ export default {
     };
   },
   watch: {
-    data: {
-      handler: function(val) {
+    fetchReady(val) {
 
-        if (this.isHugeData) {
-          if (!this.loading) {
-            const target = this.$refs['bizTable'];
-            if (target) {
-              target.reloadData(val)
-                .then(() => {
-                  this.renderLoading = false;
-                })
-                .catch(e => {
-                  this.renderLoading = false;
-                });
-            }
+      if (!val) return;
+
+      if (this.isHugeData) {
+
+        this.$nextTick(() => {
+          const target = this.$refs['bizTable'];
+          if (target) {
+            this.renderLoading = this.isHugeData;
+            target.reloadData(this.data)
+              .then(() => {
+                this.renderLoading = false;
+              })
+              .catch(e => {
+                this.renderLoading = false;
+              });
           }
-        }
-      },
-      deep: true,
+        });
+      }
     },
   },
 };
