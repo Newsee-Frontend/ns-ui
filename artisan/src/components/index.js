9a3fd1e4573759e178ef746f44a05887ac11a4f8
xilanhuadechuntian <Lensgcx@163.com>
2020-03-02 14:59:04 +0800

    Update index.js

--- artisan/src/components/index.js
+++ artisan/src/components/index.js
@@ -1,8 +1,7 @@
-import BizTabsView from './BizTabsView/BizTabsViews';
 // import BizTableV3 from './Biz-table/Biz-table-v3/Biz-table-v3.vue';
 import DemoBlock from './Demo/DemoBlock';
 
-Vue.component('biz-tabs-views', BizTabsView);
+
 Vue.component('demo-block', DemoBlock);
 // Vue.component('biz-table-v3', BizTableV3);
 
