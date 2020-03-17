51d0c5c7e50899ee1736cb47340626703c242c23
xilanhuadechuntian <Lensgcx@163.com>
2020-03-17 13:06:21 +0800

    布局组件去除 overview id

--- src/packages/Layout/Layout.js
+++ src/packages/Layout/Layout.js
@@ -16,7 +16,7 @@ export default create({
         <div class="sidebar-wrapper">{this.$slots.sidebar}</div>
 
         <div class="page-tabs-wrapper">{this.$slots['tabs-view']}</div>
-        <section id="overview" class="main-container">
+        <section class="main-container">
           {this.$slots['app-main']}
         </section>
         <div class="other">{this.$slots.other}</div>
