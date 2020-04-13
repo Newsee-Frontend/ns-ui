9af88527751dde147a7cf6a07bfefaa3a2177d49
xilanhuadechuntian <Lensgcx@163.com>
2020-04-13 15:49:55 +0800

    theme change

--- artisan/src/UI/Element-UI/index.js
+++ artisan/src/UI/Element-UI/index.js
@@ -1,6 +1,18 @@
+// import Vue from 'vue';
+// import ElementUI from 'element-ui';
+// import 'cx-theme/dist/cx-theme.min.css';
+// import '../../style/cover/element-cover/index.scss';
+//
+// Vue.use(ElementUI);
+
 import Vue from 'vue';
 import ElementUI from 'element-ui';
-import 'cx-theme/dist/cx-theme.min.css';
+
+//base style
+import './fonts/element-icons.ttf';
+import './fonts/element-icons.woff';
+import './arm-theme.min.css';
+//cover style
 import '../../style/cover/element-cover/index.scss';
 
 Vue.use(ElementUI);
