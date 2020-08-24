460b07a2939ad0f41e35b3f54a8d31ba7498dc13
xilanhuadechuntian <Lensgcx@163.com>
2020-08-24 15:06:37 +0800

    Update template.js

--- src/packages/Skiner/template.js
+++ src/packages/Skiner/template.js
@@ -2,6 +2,6 @@ export default color => {
   return `
   #side-menu-wrapper .main-menu li.main-menu__item.is-active{background-color:${color}} 
   #side-menu-wrapper .menu-item .menu-node__content.is-active .menu-node__label{ color: ${color}}
-  #Header{background-color:${color}}
+  #header-wrapper #Header{background-color:${color}}
   `;
 };
