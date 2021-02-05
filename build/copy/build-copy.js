b0ac0c11e524c3238f27b513945f669c2b8ed318
xilanhuadechuntian <Lensgcx@163.com>
2021-02-05 11:57:37 +0800

    [0.0.2-13] Upgrade to V-0.0.2-13

--- build/copy/build-copy.js
+++ build/copy/build-copy.js
@@ -1,30 +1,42 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
 const fs = require('fs-extra');
-var path = require('path');
-var notify = require('gulp-notify');
-var clean = require('gulp-clean');
+const path = require('path');
+const notify = require('gulp-notify');
+const clean = require('gulp-clean');
 
 
-var targetPath = path.join(__dirname, '../../lib');
-var toPath = '../../../CX-WAP-Design/';
-var fileName = 'cx-wap-ui';
+const targetPath = path.join(__dirname, '../../lib');
+const toPath = '../../../CX-WAP-Design/';
+const fileName = 'cx-wap-ui';
 
-gulp.task('clean-lib', function () {
-  gulp.src(toPath + fileName, {read: false})
-    .pipe(clean({force: true}))
-    .pipe(notify({message: '===== clean lib complete ====='}));
+gulp.task('clean-lib', gulp.series(() => {
+    return new Promise(function (resolve, reject) {
 
-});
+      gulp.src(toPath + fileName, {read: false})
+        .pipe(clean({force: true}))
+        .pipe(notify({message: '===== clean lib complete ====='}));
+
+      resolve();
+
+    });
+  },
+));
 
 // copy
-gulp.task('copy-lib', function () {
-  // copy
-  fs.copySync(targetPath, toPath + fileName);
+gulp.task('copy-lib', gulp.series(() => {
+    return new Promise(function (resolve, reject) {
 
-  // gulp.src(targetPath + '/**/*')
-  //   .pipe(gulp.dest(toPath + fileName))
-  //   .pipe(notify({message: '===== lib copy complete ====='}));
-});
+      // copy
+      fs.copySync(targetPath, toPath + fileName);
+
+      // gulp.src(targetPath + '/**/*')
+      //   .pipe(gulp.dest(toPath + fileName))
+      //   .pipe(notify({message: '===== lib copy complete ====='}));
+
+      resolve();
+    });
+  },
+));
 
 
-gulp.task('default', ['clean-lib', 'copy-lib']);
+gulp.task('default', gulp.series(['clean-lib', 'copy-lib']));
--- build/style-operate/compile-style.js
+++ build/style-operate/compile-style.js
@@ -11,41 +11,59 @@ const options = minimist(process.argv.slice(2));
 
 
 // compile component css
-gulp.task('compile-less', () => (
-  gulp
-    .src(options.lesspath.split(','))
-    .pipe(gulpLess({
-      paths: [path.resolve(__dirname, 'node_modules')],
-    }))
-    .pipe(postcss())
-    .pipe(csso())
-    .pipe(gulp.dest(file => file.base.replace('.less', '.css')))
+gulp.task('compile-less', gulp.series(() => {
+    return new Promise(function (resolve, reject) {
+
+      gulp.src(options.lesspath.split(','))
+        .pipe(gulpLess({
+          paths: [path.resolve(__dirname, 'node_modules')],
+        }))
+        .pipe(postcss())
+        .pipe(csso())
+        .pipe(gulp.dest(file => file.base.replace('.less', '.css')))
+      resolve();
+
+    });
+  },
 ));
 
 // compile component css
-gulp.task('compile-scss', () => {
-  gulp
-    .src(options.scsspath.split(','))
-    .pipe(gulpSass({
-      paths: [path.resolve(__dirname, 'node_modules')],
-    }))
-    .pipe(postcss())
-    .pipe(csso())
-    .pipe(gulp.dest(file => file.base.replace('.scss', '.css')));
-});
+gulp.task('compile-scss', gulp.series(() => {
+    return new Promise(function (resolve, reject) {
+
+      gulp.src(options.scsspath.split(','))
+        .pipe(gulpSass({
+          paths: [path.resolve(__dirname, 'node_modules')],
+        }))
+        .pipe(postcss())
+        .pipe(csso())
+        .pipe(gulp.dest(file => file.base.replace('.scss', '.css')));
+      resolve();
+
+    });
+  },
+));
 
 // copy
-gulp.task('copyicons', function() {
-  // copy
-  fs.copySync(path.join(__dirname, '../../src/style/icons'), '../../lib/icons');
-  fs.copySync(path.join(__dirname, '../../src/style/icons'), '../../es/icons');
-  
-  // gulp.src(targetPath + '/**/*')
-  //   .pipe(gulp.dest(toPath + fileName))
-  //   .pipe(notify({message: '===== lib copy complete ====='}));
-});
+gulp.task('copyicons', gulp.series(() => {
+    return new Promise(function (resolve, reject) {
+      // copy
+      fs.copySync(path.join(__dirname, '../../src/style/icons'), '../../lib/icons');
+      fs.copySync(path.join(__dirname, '../../src/style/icons'), '../../es/icons');
+
+      // gulp.src(targetPath + '/**/*')
+      //   .pipe(gulp.dest(toPath + fileName))
+      //   .pipe(notify({message: '===== lib copy complete ====='}));
+      resolve();
+    });
+  },
+));
 
 
-gulp.task('default', ['compile-scss', 'compile-less', 'copyicons']);
+gulp.task('default', gulp.series([
+  'compile-scss',
+  // 'compile-less',
+  'copyicons'
+]));
 
 
