2bbd1203fc4671d5854b8021519fb63afcac2f87
txj <1943418084@qq.com>
2023-06-19 16:24:17 +0800

    V10-13302 【组件文档】新视窗前端组件文档 ——Badge 标记，Pagination 分页，Progress 进度条增加属性、事件

--- src/packages/Progress/Progress.js
+++ src/packages/Progress/Progress.js
@@ -6,40 +6,66 @@ export default create({
     return {};
   },
   props: {
-    //百分比（必填） 0-100
+    /**
+     * 百分比（必填)
+     * @values 0-100
+     */
     percentage: {
       type: Number,
       default: 0,
     },
-    //进度条类型 separator/circle
+
+    /**
+     * 进度条类型
+     * @values 'line','circle','dashboard'
+     */
     type: {
       type: String,
       default: 'line',
     },
-    //进度条的宽度，单位 px
+
+    /**
+     * 进度条的宽度，单位 px
+     */
     strokeWidth: {
       type: Number,
       default: 6,
     },
-    //进度条显示文字内置在进度条内（只在 type=separator 时可用）
+
+    /**
+     * 进度条显示文字内置在进度条内（只在 type=line 时可用）
+     */
     textInside: {
       type: Boolean,
       default: false,
     },
-    //进度条当前状态 success/exception
+
+    /**
+     * 进度条当前状态
+     * @values 'success','exception','warning'
+     */
     status: {
       type: String,
     },
-    //进度条背景色（会覆盖 status 状态颜色）
+
+    /**
+     * 进度条背景色（会覆盖 status 状态颜色）
+     */
     color: {
       type: [Function, String, Array],
     },
-    //环形进度条画布宽度（只在 type=circle 时可用）
+
+    /**
+     * 环形进度条画布宽度（只在 type 为 circle 或 dashboard 时可用）
+     */
     width: {
       type: Number,
       default: 126,
     },
-    //是否显示进度条文字内容
+
+    /**
+     * 是否显示进度条文字内容
+     */
     showText: {
       type: Boolean,
       default: true,
